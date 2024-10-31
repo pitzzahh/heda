85d2e7337e1dfdbb3d27715cf5265957a297e74a
Peter John Arao <pitzzahh@gmail.com>
2024-10-31 21:46:24 +0800

    ✨ Implement dialog components for enhanced user interaction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-	import { Button } from '@/components/ui/button/index.js';
+	import { Button, buttonVariants } from '@/components/ui/button/index.js';
+	import * as Dialog from '@/components/ui/dialog/index.js';
 </script>
 
-<Button>Click me</Button>
+<div class="flex min-h-screen items-center justify-center bg-background">
+	<div class="flex flex-col gap-4">
+		<Dialog.Root>
+			<Dialog.Trigger class={buttonVariants({ size: '2xl' })}>New File</Dialog.Trigger>
+			<Dialog.Content>
+				<Dialog.Header>
+					<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+					<Dialog.Description>
+						This action cannot be undone. This will permanently delete your account and remove your
+						data from our servers.
+					</Dialog.Description>
+				</Dialog.Header>
+				<Button href="/home" class="w-full">Save</Button>
+			</Dialog.Content>
+		</Dialog.Root>
+		<Button variant="ghost" size="xl" class="w-full">Load File</Button>
+	</div>
+</div>
