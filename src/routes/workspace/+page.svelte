489c0942e544d9c56bccc032dddfb955c1c1e871
Peter John Arao <pitzzahh@gmail.com>
2024-11-10 14:18:16 +0800

    ✨ Refactor workspace page to integrate HighestUnitForm and streamline props handling

--- src/routes/workspace/+page.svelte
+++ src/routes/workspace/+page.svelte
@@ -1,35 +1,27 @@
 <script lang="ts">
+	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
 	import { AppSidebar } from '@/components/custom/sidebar';
 	import * as Breadcrumb from '@/components/ui/breadcrumb/index.js';
 	import { Separator } from '@/components/ui/separator/index.js';
 	import * as Sidebar from '@/components/ui/sidebar/index.js';
-	import { LocalStorage } from '@/hooks/storage.svelte';
 	import { Button, buttonVariants } from '@/components/ui/button/index.js';
 	import * as Dialog from '@/components/ui/dialog/index.js';
-	import { Input } from '@/components/ui/input/index.js';
-	import { Label } from '@/components/ui/label/index.js';
 	import { toast } from 'svelte-sonner';
+	import { HighestUnitForm } from '@/components/custom';
 
 	interface Props {
 		is_new_file: boolean;
 		is_load_file: boolean;
+		data: PageData;
 	}
 
-	let { is_new_file, is_load_file }: Props = $props();
+	let { is_new_file, is_load_file, data }: Props = $props();
 
-	let highest_unit = $state(false);
-
-	type Box = {
-		color: string;
-		dimensions: number[];
-	};
+	let highest_unit = $state(is_new_file);
 
 	onMount(() => {
 		toast.info(`Is new file: ${is_new_file}\nIs load file: ${is_load_file}`);
-		if (is_new_file) {
-			highest_unit = true;
-		}
 	});
 </script>
 
@@ -59,23 +51,14 @@
 </Sidebar.Provider>
 
 <Dialog.Root bind:open={highest_unit}>
-	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Edit Profile</Dialog.Trigger>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Highest unit form</Dialog.Trigger>
 	<Dialog.Content class="sm:max-w-[425px]">
 		<Dialog.Header>
 			<Dialog.Title class="text-center font-bold"
 				>Choose the highest unit for this project.</Dialog.Title
 			>
 		</Dialog.Header>
-		<div class="grid gap-4 py-4">
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="name" class="text-right">Name</Label>
-				<Input id="name" value="Pedro Duarte" class="col-span-3" />
-			</div>
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="username" class="text-right">Username</Label>
-				<Input id="username" value="@peduarte" class="col-span-3" />
-			</div>
-		</div>
+		<HighestUnitForm highest_unit_form={data.highest_unit_form} />
 		<Dialog.Footer>
 			<Button type="submit">Save changes</Button>
 		</Dialog.Footer>
