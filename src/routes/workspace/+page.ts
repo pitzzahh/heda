fed55b2ea126cf4305eefadff6458874021fe2a1
Peter John Arao <pitzzahh@gmail.com>
2024-11-10 14:18:24 +0800

    ✨ Integrate highest_unit_schema validation in page load function

--- src/routes/workspace/+page.ts
+++ src/routes/workspace/+page.ts
@@ -1,4 +1,6 @@
-import type { PageLoad } from './$types';
+import { highest_unit_schema } from '@/schema';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = (async ({ url: { searchParams } }) => {
   // get if new file or load file
@@ -6,6 +8,7 @@ export const load = (async ({ url: { searchParams } }) => {
   const is_load_file = searchParams.get('load_file') === 'true';
   return {
     is_new_file,
-    is_load_file
+    is_load_file,
+    highest_unit_form: await superValidate(zod(highest_unit_schema))
   };
-}) satisfies PageLoad;
+});
