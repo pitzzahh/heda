b460754575c9e86ff71600bea30c79a0728186cc
Peter John Arao <pitzzahh@gmail.com>
2024-11-10 15:34:58 +0800

    ✨ Refactor highest_unit_schema to import constants and add wire length validation

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,16 +1,9 @@
 import { z } from 'zod';
 
-const ambient_temperatures = [
-  { label: '60', value: '60' },
-  { label: '75', value: '75' },
-  { label: '90', value: '90' },
-  { label: 'temparature limitation', value: 'default' }
-] as const;
-
-type Temperature = (typeof ambient_temperatures)[number]['value'];
+import { ambient_temperatures, FIELD_VALIDATION } from '@/constants';
+import type { Temperature } from '@/types/misc';
 
 export const highest_unit_schema = z.object({
-  id: z.string().optional(),
   distribution_unit: z.string().refine((v) => v, { message: 'A distribution unit is required.' }),
   ambient_temp: z.enum(
     ambient_temperatures.map((f) => f.value) as [Temperature, ...Temperature[]],
@@ -18,8 +11,14 @@ export const highest_unit_schema = z.object({
       errorMap: () => ({ message: 'Please select a valid ambient temperature.' })
     }
   ),
-  phase_name: z.enum(['one_phase', 'three_phase_wye', 'three_phase_delta'], {
-    required_error: 'You need to select a phase name'
+  wire_length: z
+    .string()
+    .refine(FIELD_VALIDATION.TEST.ALL_NUMBER, 'Wire length must be a number')
+    .refine((value) => Number(value) > 0, {
+      message: 'Wire must be greater than 0.'
+    }),
+  phase: z.enum(['one_phase', 'three_phase_wye', 'three_phase_delta'], {
+    required_error: 'You need to select a phase'
   })
 });
 export type HighestUnitSchema = z.infer<typeof highest_unit_schema>;
