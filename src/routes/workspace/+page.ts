08c20030b89615d857e1489e65733bfb4c35d032
Peter John Arao <pitzzahh@gmail.com>
2024-11-10 15:36:26 +0800

    ✨ Simplify loading logic by inlining new file and load file checks

--- src/routes/workspace/+page.ts
+++ src/routes/workspace/+page.ts
@@ -3,12 +3,9 @@ import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = (async ({ url: { searchParams } }) => {
-  // get if new file or load file
-  const is_new_file = searchParams.get('new_file') === 'true';
-  const is_load_file = searchParams.get('load_file') === 'true';
   return {
-    is_new_file,
-    is_load_file,
+    is_new_file: searchParams.get('new_file') === 'true',
+    is_load_file: searchParams.get('load_file') === 'true',
     highest_unit_form: await superValidate(zod(highest_unit_schema))
   };
 });
