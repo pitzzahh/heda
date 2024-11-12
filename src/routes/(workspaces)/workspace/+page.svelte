b45cc1329f55787f2b771032e21c41ceb0794b7c
Peter John Arao <pitzzahh@gmail.com>
2024-11-12 21:25:21 +0800

    ✨ Add OnePhaseMainLoadForm dialog to workspace page with file selection prompt

--- src/routes/(workspaces)/workspace/+page.svelte
+++ src/routes/(workspaces)/workspace/+page.svelte
@@ -1,8 +1,30 @@
-<script>
-	import { FileQuestionIcon } from 'lucide-svelte';
+<script lang="ts">
+	import { FileQuestionIcon } from '@/assets/icons/lucide';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { OnePhaseMainLoadForm } from '@/components/custom/load';
+
+	let { data } = $props();
 </script>
 
 <div class="grid size-full place-content-center place-items-center gap-2">
 	<FileQuestionIcon class="size-24 text-muted-foreground" />
 	<h2 class="text-xl font-semibold text-muted-foreground">No File Selected</h2>
 </div>
+
+<Dialog.Root>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Test Main Load Form</Dialog.Trigger
+	>
+	<Dialog.Content class="sm:max-w-fit">
+		<Dialog.Header>
+			<Dialog.Title>Test Main Load Form</Dialog.Title>
+			<Dialog.Description>
+				Make changes to your profile here. Click save when you're done.
+			</Dialog.Description>
+		</Dialog.Header>
+		<OnePhaseMainLoadForm one_phase_main_load_form={data.one_phase_main_load_form} />
+		<Dialog.Footer>
+			<Button type="submit">Save changes</Button>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
