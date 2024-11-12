329e892f3c9c26f5408550c1f7c7778769c9b79d
Peter John Arao <pitzzahh@gmail.com>
2024-11-12 21:57:43 +0800

    ✨ Add Special type to index for enhanced type definitions

--- src/lib/types/misc/index.ts
+++ src/lib/types/misc/index.ts
@@ -1,3 +1,5 @@
-import type { ambient_temperatures } from "@/constants";
+import type { ambient_temperatures, specials } from "@/constants";
 
 export type Temperature = (typeof ambient_temperatures)[number]['value'];
+
+export type Special = (typeof specials)[number]['value'];
