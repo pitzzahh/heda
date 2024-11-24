de6b1f2e9eb4a4d588e87d7b3fc7f191e5caad83
Peter John Arao <pitzzahh@gmail.com>
2024-11-24 10:56:17 +0800

    ➕ Implement SQLite database integration with Drizzle ORM and Tauri SQL plugin, including schema definition for 'load' table

---
+++ src/lib/db/schema/load.ts
@@ -0,0 +1,14 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+const load = sqliteTable("load", {
+  age: integer("age").default(18),
+  // city: text("city").default("NULL"),
+  // created_at: text("created_at").default("CURRENT_TIMESTAMP"),
+  // deleted_at: text("deleted_at").default("NULL"),
+  // email: text("email").unique(),
+  id: integer("id").primaryKey().unique(),
+  name: text("name"),
+  // updated_at: text("updated_at").default("CURRENT_TIMESTAMP"),
+});
+
+export default load;
