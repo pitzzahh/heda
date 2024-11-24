ba618deb942c186ec94e4ccca90a55df78827871
Peter John Arao <pitzzahh@gmail.com>
2024-11-24 10:51:02 +0800

    ➕ Add drizzle.config.ts for Drizzle ORM configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/lib/db/schema/index.ts",
+  verbose: false,
+  strict: true,
+  out: "./src-tauri/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: "sqlite:test.db"
+  }
+});
