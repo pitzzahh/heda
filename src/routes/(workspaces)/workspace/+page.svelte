02b9c5e2ed5daef728aa84bde09f00635dc6dd7b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-11-25 17:56:01 +0800

    ✨ Remove Test Main Load Form dialog from workspace page

--- src/routes/(workspaces)/workspace/+page.svelte
+++ src/routes/(workspaces)/workspace/+page.svelte
@@ -12,14 +12,4 @@
 	<h2 class="text-xl font-semibold text-muted-foreground">No File Selected</h2>
 </div>
 
-<Dialog.Root>
-	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Test Main Load Form</Dialog.Trigger
-	>
-	<Dialog.Content class="max-w-[70%]">
-		<Dialog.Header>
-			<Dialog.Title>Test Main Load Form</Dialog.Title>
-			<Dialog.Description>Enter the load details.</Dialog.Description>
-		</Dialog.Header>
-		<GenericPhaseMainLoadForm phase_main_load_form={data.phase_main_load_form} />
-	</Dialog.Content>
-</Dialog.Root>
+
