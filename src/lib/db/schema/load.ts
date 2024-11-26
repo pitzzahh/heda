7475719557bb4db94fa307ec43e033e85fd2a589
Peter John Arao <pitzzahh@gmail.com>
2024-11-26 14:30:36 +0800

    ✨ Update 'load' table schema: add new fields for load description, quantity, varies, continuous, and special; set default values for some fields.

--- src/lib/db/schema/load.ts
+++ src/lib/db/schema/load.ts
@@ -1,14 +1,15 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 const load = sqliteTable("load", {
-  age: integer("age").default(18),
-  // city: text("city").default("NULL"),
+  id: integer("id").primaryKey().unique(),
+  load_description: text("load_description").default("NULL"),
+  quantity: integer("quantity"),
+  varies: integer("varies"),
   // created_at: text("created_at").default("CURRENT_TIMESTAMP"),
   // deleted_at: text("deleted_at").default("NULL"),
-  // email: text("email").unique(),
-  id: integer("id").primaryKey().unique(),
-  name: text("name"),
   is_panel: integer("is_panel").default(0),
+  continuous: integer("continuous").notNull().default(0),
+  special: text("special").default("NULL"),
   // updated_at: text("updated_at").default("CURRENT_TIMESTAMP"),
 });
 
