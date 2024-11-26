87754bdde56efa3d58c736d94eda0769ca41d642
Peter John Arao <pitzzahh@gmail.com>
2024-11-26 15:15:59 +0800

    ✨ Add Tauri file system plugin and implement database migration functionality: include plugin in package.json, create migration logic, and update main application to utilize new features.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 fn main() {
   tauri::Builder::default()
       .plugin(tauri_plugin_sql::Builder::default().build())
+      .plugin(tauri_plugin_fs::init())
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,16 +1,88 @@
-import { createClient } from '@libsql/client/sqlite3';
-import { drizzle } from 'drizzle-orm/libsql';
-import { seed } from 'drizzle-seed';
+// import { createClient } from '@libsql/client/sqlite3';
+// import { drizzle } from 'drizzle-orm/libsql';
+// import { seed } from 'drizzle-seed';
+// import * as schema from '@/db/schema';
+
+// export async function createDatabase(file_name: string, extension: string = 'sqlite') {
+//   const client = createClient({
+//     url: `file:${process.cwd()}/${file_name}.${extension}`,
+//   });
+
+//   const db = drizzle(client);
+
+//   const seed_result = await seed(db, schema, { count: 20 });
+//   console.log('seed_result', seed_result);
+//   return db
+// }
+
+import { drizzle } from "drizzle-orm/sqlite-proxy";
+import Database from "tauri-plugin-sql-api";
 import * as schema from '@/db/schema';
 
-export async function createDatabase(file_name: string, extension: string = 'sqlite') {
-  const client = createClient({
-    url: `file:${process.cwd()}/${file_name}.${extension}`,
-  });
+/**
+ * Represents the result of a SELECT query.
+ */
+export type SelectQueryResult = {
+  [key: string]: any;
+};
 
-  const db = drizzle(client);
+/**
+ * Loads the sqlite database via the Tauri Proxy.
+ * @param fileName The name of the database file.
+ * @param fileExtension The extension of the database file.
+ */
+export async function loadDatabase(fileName: string = "sqlite-1", fileExtension: string = "db") {
+  const dbPath = `sqlite:${fileName}.${fileExtension}`;
+  return await Database.load(dbPath);
+}
 
-  const seed_result = await seed(db, schema, { count: 20 });
-  console.log('seed_result', seed_result);
-  return db
+/**
+ * The sqlite database instance.
+ */
+export const sqlite = await loadDatabase();
+
+/**
+ * The drizzle database instance.
+ */
+export const db = drizzle<typeof schema>(
+  async (sql, params, method) => {
+    let rows: any = [];
+    let results = [];
+
+    // If the query is a SELECT, use the select method
+    if (isSelectQuery(sql)) {
+      rows = await sqlite.select(sql, params).catch((e) => {
+        console.error("SQL Error:", e);
+        return [];
+      });
+    } else {
+      // Otherwise, use the execute method
+      rows = await sqlite.execute(sql, params).catch((e) => {
+        console.error("SQL Error:", e);
+        return [];
+      });
+      return { rows: [] };
+    }
+
+    rows = rows.map((row: any) => {
+      return Object.values(row);
+    });
+
+    // If the method is "all", return all rows
+    results = method === "all" ? rows : rows[0];
+
+    return { rows: results };
+  },
+  // Pass the schema to the drizzle instance
+  { schema: schema, logger: true }
+);
+
+/**
+ * Checks if the given SQL query is a SELECT query.
+ * @param sql The SQL query to check.
+ * @returns True if the query is a SELECT query, false otherwise.
+ */
+function isSelectQuery(sql: string): boolean {
+  const selectRegex = /^\s*SELECT\b/i;
+  return selectRegex.test(sql);
 }
