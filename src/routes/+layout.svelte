571b79b2554f020ba1770997ead672619bdcfe64
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-11-28 00:02:42 +0800

    ✨ Add global color theme management and integrate into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, mode } from 'mode-watcher';
 	import { Toaster } from '@/components/ui/sonner/index.js';
 	import { setState } from '@/state/index.svelte';
 	import type { MiscState } from '@/state/types';
 	import { MISC_STATE_CTX } from '@/state/constants';
 	import SuperDebug from 'sveltekit-superforms';
 	import { dev } from '$app/environment';
+	import { LocalStorage } from '@/hooks/storage.svelte';
+	import setGlobalColorTheme from '@/theme-colors/theme-colors';
 
 	let { children } = $props();
+	const localStorage = new LocalStorage<{ color: 'autocad' | 'excel' }>('theme_color');
+	const themeColor = localStorage?.current?.color;
 
 	const miscState = setState<MiscState>(
 		{
@@ -16,6 +20,10 @@
 		},
 		MISC_STATE_CTX
 	);
+
+	$effect(() => {
+		setGlobalColorTheme($mode === 'light' ? 'light' : 'dark', themeColor ? themeColor : 'excel');
+	});
 </script>
 
 <Toaster richColors={true} />
