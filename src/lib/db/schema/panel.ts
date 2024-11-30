f33f281dad25fb314099af3df6b1825f3ddff8e0
Peter John Arao <pitzzahh@gmail.com>
2024-11-30 22:33:00 +0800

    🔥 Remove unused database-related files and schemas to streamline the codebase

--- src/lib/db/schema/panel.ts
+++
@@ -1,8 +0,0 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-const panel = sqliteTable("panel", {
-  id: integer("id").primaryKey().unique(),
-  name: text("name").notNull(),
-});
-
-export default panel;
