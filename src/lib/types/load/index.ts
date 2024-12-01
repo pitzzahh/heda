a92a3217463038cc69ae9cc3d1f72d1958212e07
Peter John Arao <pitzzahh@gmail.com>
2024-12-01 21:01:26 +0800

    🏷️ Add optional loads property to Load type for enhanced data representation

--- src/lib/types/load/index.ts
+++ src/lib/types/load/index.ts
@@ -1,11 +1,12 @@
 export type LoadType = "DEFAULT" | 'CUSTOM';
 
 export type Load = {
-  id: string,
+  id: string;
   load_description: string;
   quantity: number;
   varies: number;
   is_panel: number;
   continuous: number;
   special: string;
-}
+  loads?: Load[];
+};
