41b9a174638376a5c8a2e54e8277c55321fd59f3
Peter John Arao <pitzzahh@gmail.com>
2024-12-04 01:15:33 +0800

    ✨ Refactor panel schema to improve naming and utilize constants for phase types

--- src/lib/schema/panel/index.ts
+++ src/lib/schema/panel/index.ts
@@ -1,25 +1,23 @@
 import { z } from 'zod';
+import type { Phase, PhaseType } from '@/types/phase';
+import { DEFAULT_PHASES_ENUMS, DEFAULT_THREE_PHASE_TYPES_ENUMS } from '@/constants';
 
-export const generic_panel_schema = z.object({
-  main_id: z.string().refine((v) => v, { message: 'A main ID is required.' }),
-  main_phase: z.enum(['one_phase', 'three_phase_wye', 'three_phase_delta'], {
-    required_error: 'You need to select a phase'
-  }),
+export const generic_phase_panel_schema = z.object({
   name: z.string().refine((v) => v, { message: 'A panel name is required.' }),
   circuit_number: z
     .number({ message: "Please enter a valid circuit number." })
     .refine((value) => value > 0, {
       message: 'Circuit number must be greater than 0.'
     }),
-  panel_ambient_temperature: z
+  ambient_temperature: z
     .string()
     .refine((v) => v, { message: 'A panel ambient temperature is required.' }),
-  panel_type: z.enum(['wye', 'delta', 'algo'], {
+  type: z.enum(DEFAULT_THREE_PHASE_TYPES_ENUMS.map((v) => v.value) as [PhaseType, ...PhaseType[]], {
     required_error: 'You need to select a 3P type'
   }).optional().nullable(),
-  panel_phase: z.enum(['one_phase', 'three_phase_wye', 'three_phase_delta'], {
+  phase: z.enum(DEFAULT_PHASES_ENUMS.map((v) => v.value) as [Phase, ...Phase[]], {
     required_error: 'You need to select a phase'
   }),
 });
 
-export type GenericPanelSchema = z.infer<typeof generic_panel_schema>;
+export type GenericPhasePanelSchema = z.infer<typeof generic_phase_panel_schema>;
