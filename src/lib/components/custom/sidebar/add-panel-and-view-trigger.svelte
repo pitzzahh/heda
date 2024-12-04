3381a82cb6076c8f5416ecb40d185899c6186ded
Peter John Arao <pitzzahh@gmail.com>
2024-12-04 10:34:34 +0800

    ♻️ Update add-panel-and-view-trigger component to use SuperValidated type and integrate GenericPhaseMainPanelForm for improved panel addition functionality

--- src/lib/components/custom/sidebar/add-panel-and-view-trigger.svelte
+++ src/lib/components/custom/sidebar/add-panel-and-view-trigger.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
-	import type { Panel } from '@/types/panel';
 	import { getProjectState } from '@/hooks/project.svelte';
 	import { goto } from '$app/navigation';
 	import type { GenericPhasePanelSchema } from '@/schema/panel';
+	import type { SuperValidated } from 'sveltekit-superforms';
+	import { GenericPhaseMainPanelForm } from '@/components/custom/panel';
 
 	let {
 		children,
 		id,
 		generic_phase_panel_form
-	}: { children: any; id: string; generic_phase_panel_form: GenericPhasePanelSchema } =
-		$props();
+	}: {
+		children: any;
+		id: string;
+		generic_phase_panel_form: SuperValidated<GenericPhasePanelSchema>;
+	} = $props();
 	let panelName = $state('');
 	let isDialogOpen = $state(false); // Add a reactive variable to control the dialog state
 	let projectState = getProjectState();
@@ -74,21 +75,15 @@
 	}
 </script>
 
-<button class="flex items-center gap-2" onclick={handleClick}>
+<button class="flex w-full items-center gap-2" onclick={handleClick}>
 	{@render children?.()}
 </button>
 
 <Dialog.Root bind:open={isDialogOpen}>
-	<Dialog.Content class="sm:max-w-[425px]">
+	<Dialog.Content class="max-w-[70%]">
 		<Dialog.Header>
 			<Dialog.Title>Add a Panel</Dialog.Title>
 		</Dialog.Header>
-		<div class="grid gap-2">
-			<Label for="name" class="text-left">Name</Label>
-			<Input id="name" bind:value={panelName} />
-		</div>
-		<Dialog.Footer>
-			<Button type="submit" class="w-full" onclick={handleSubmit}>Add</Button>
-		</Dialog.Footer>
+		<GenericPhaseMainPanelForm {generic_phase_panel_form} main_phase="THREE_PHASE_WYE" />
 	</Dialog.Content>
 </Dialog.Root>
