7cdb0667fa4cd9e715ee303376678bf8da72cb8b
Peter John Arao <pitzzahh@gmail.com>
2024-12-04 10:32:11 +0800

    ♻️ Refactor generic phase main panel form to improve variable naming and structure

--- src/lib/components/custom/panel/generic-phase-main-panel-form.svelte
+++ src/lib/components/custom/panel/generic-phase-main-panel-form.svelte
@@ -25,14 +25,13 @@
 	import type { Phase } from '@/types/phase';
 
 	interface Props {
-		generic_phase_main_panel_form: T;
+		generic_phase_panel_form: T;
 		main_phase: Phase;
-		saved_path?: string;
 	}
 
-	let { generic_phase_main_panel_form, main_phase }: Props = $props();
+	let { generic_phase_panel_form, main_phase }: Props = $props();
 
-	const form = superForm(generic_phase_main_panel_form, {
+	const form = superForm(generic_phase_panel_form, {
 		SPA: true,
 		validators: zodClient(generic_phase_panel_schema),
 		onUpdate: ({ form }) => {
@@ -78,100 +77,101 @@
 
 <form method="POST" use:enhance>
 	<div class="grid grid-cols-2 place-items-start justify-between gap-2">
-		<Form.Field {form} name="name">
-			<Form.Control>
-				{#snippet children({ props })}
-					<Form.Label>Name</Form.Label>
-					<Input {...props} bind:value={$formData.name} placeholder="Enter panel name" />
-				{/snippet}
-			</Form.Control>
-			<Form.Description>This is the panel name.</Form.Description>
-			<Form.FieldErrors />
-		</Form.Field>
-		<Form.Field {form} name="circuit_number">
-			<Form.Control>
-				{#snippet children({ props })}
-					<Form.Label>Circuit number</Form.Label>
-					<Input
-						{...props}
-						type="number"
-						inputmode="numeric"
-						min={1}
-						bind:value={$formData.circuit_number}
-						placeholder="Enter the circuit number"
-					/>
-				{/snippet}
-			</Form.Control>
-			<Form.Description>
-				This is the circuit number that will determine the circuit number of the wire to the main.
-			</Form.Description>
-			<Form.FieldErrors />
-		</Form.Field>
-		<Form.Field {form} name="ambient_temperature" class="mt-2.5 flex flex-col">
-			<Popover.Root bind:open={open_ambient_temp}>
-				<Form.Control id={ambient_temp_trigger_id}>
+		<div>
+			<Form.Field {form} name="name">
+				<Form.Control>
 					{#snippet children({ props })}
-						<Form.Label>Ambient Temperature</Form.Label>
-						<Popover.Trigger
-							class={cn(
-								buttonVariants({ variant: 'outline' }),
-								'justify-between',
-								!$formData.ambient_temperature && 'text-muted-foreground'
-							)}
-							role="combobox"
-							{...props}
-						>
-							{ambient_temperatures.find((f) => f.value === $formData.ambient_temperature)?.label ??
-								'Select an ambient temperature'}
-							<CaretSort class="ml-2 size-4 shrink-0 opacity-50" />
-						</Popover.Trigger>
-						<input hidden value={$formData.ambient_temperature} name={props.name} />
+						<Form.Label>Name</Form.Label>
+						<Input {...props} bind:value={$formData.name} placeholder="Enter panel name" />
 					{/snippet}
 				</Form.Control>
-				<Popover.Content class="w-auto p-0">
-					<Command.Root>
-						<Command.Input autofocus placeholder="Search an ambient temp..." class="h-9" />
-						<Command.Empty>No ambient temp found.</Command.Empty>
-						<Command.Group>
-							{#each ambient_temperatures as ambient_temp}
-								<Command.Item
-									value={ambient_temp.value}
-									onSelect={() => {
-										$formData.ambient_temperature = ambient_temp.value;
-										closeAndFocusTrigger(ambient_temp_trigger_id);
-									}}
-								>
-									{ambient_temp.label}
-									<Check
-										class={cn(
-											'ml-auto size-4',
-											ambient_temp.value !== $formData.ambient_temperature && 'text-transparent'
-										)}
-									/>
-								</Command.Item>
-							{/each}
-						</Command.Group>
-					</Command.Root>
-				</Popover.Content>
-			</Popover.Root>
-			<Form.Description>
-				This is the ambient temp that will determine the ambient temp of the panel wire to the main.
-			</Form.Description>
-			<Form.FieldErrors />
-		</Form.Field>
-	</div>
-	<div class="flex flex-col items-center gap-1">
-		{#if main_phase !== 'ONE_PHASE'}
-			<div class="row-start-1">
-				{@render PanelPhase()}
-			</div>
-		{/if}
-
-		<div class="row-start-2">
-			{@render PanelType()}
+				<Form.Description>This is the panel name.</Form.Description>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="circuit_number">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Circuit number</Form.Label>
+						<Input
+							{...props}
+							type="number"
+							inputmode="numeric"
+							min={1}
+							bind:value={$formData.circuit_number}
+							placeholder="Enter the circuit number"
+						/>
+					{/snippet}
+				</Form.Control>
+				<Form.Description>
+					This is the circuit number that will determine the circuit number of the wire to the main.
+				</Form.Description>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="ambient_temperature" class="mt-2.5 flex flex-col">
+				<Popover.Root bind:open={open_ambient_temp}>
+					<Form.Control id={ambient_temp_trigger_id}>
+						{#snippet children({ props })}
+							<Form.Label>Ambient Temperature</Form.Label>
+							<Popover.Trigger
+								class={cn(
+									buttonVariants({ variant: 'outline' }),
+									'justify-between',
+									!$formData.ambient_temperature && 'text-muted-foreground'
+								)}
+								role="combobox"
+								{...props}
+							>
+								{ambient_temperatures.find((f) => f.value === $formData.ambient_temperature)
+									?.label ?? 'Select an ambient temperature'}
+								<CaretSort class="ml-2 size-4 shrink-0 opacity-50" />
+							</Popover.Trigger>
+							<input hidden value={$formData.ambient_temperature} name={props.name} />
+						{/snippet}
+					</Form.Control>
+					<Popover.Content class="w-auto p-0">
+						<Command.Root>
+							<Command.Input autofocus placeholder="Search an ambient temp..." class="h-9" />
+							<Command.Empty>No ambient temp found.</Command.Empty>
+							<Command.Group>
+								{#each ambient_temperatures as ambient_temp}
+									<Command.Item
+										value={ambient_temp.value}
+										onSelect={() => {
+											$formData.ambient_temperature = ambient_temp.value;
+											closeAndFocusTrigger(ambient_temp_trigger_id);
+										}}
+									>
+										{ambient_temp.label}
+										<Check
+											class={cn(
+												'ml-auto size-4',
+												ambient_temp.value !== $formData.ambient_temperature && 'text-transparent'
+											)}
+										/>
+									</Command.Item>
+								{/each}
+							</Command.Group>
+						</Command.Root>
+					</Popover.Content>
+				</Popover.Root>
+				<Form.Description>
+					This is the ambient temp that will determine the ambient temp of the panel wire to the
+					main.
+				</Form.Description>
+				<Form.FieldErrors />
+			</Form.Field>
+		</div>
+		<div class="w-full">
+			{#if main_phase !== 'ONE_PHASE'}
+				<div class="row-end-2">
+					{@render PanelType()}
+				</div>
+				<div class="row-end-3">
+					{@render PanelPhase()}
+				</div>
+			{/if}
 		</div>
 	</div>
-
 	<Form.Button class="w-full">Save</Form.Button>
 </form>
 
