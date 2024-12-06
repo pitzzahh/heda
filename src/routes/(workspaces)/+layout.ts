8f146c1b8466103c4b57bed4deb9556497a72389
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-06 19:14:05 +0800

    ✨ Refactor load function to utilize getCurrentProject and getChildNodesByParentId for improved project and node retrieval

--- src/routes/(workspaces)/+layout.ts
+++ src/routes/(workspaces)/+layout.ts
@@ -3,6 +3,7 @@ import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { databaseInstance } from '@/db';
 import { generic_phase_panel_schema } from '@/schema/panel';
+import { getChildNodesByParentId, getCurrentProject } from '@/db/queries/index.js';
 
 export const load = async ({ url: { searchParams } }) => {
 	console.log('INIT DB');
@@ -11,98 +12,149 @@ export const load = async ({ url: { searchParams } }) => {
 
 	console.log(database);
 
-	const query = database.projects.find();
-	const existingProject = await query.exec();
+	// const query = database.projects.find();
+	// const existingProject = await query.exec();
 
-	if (!existingProject.length) {
-		// Create items
-		await database.items.insert({
-			id: 'item1',
-			node_type: 'panel',
-			panel_data: {
-				name: 'Main Panel',
-				circuit_number: 1,
-				ambient_temperature: '25C',
-				phase: 'Single'
-			},
-			load_data: undefined,
-			parent_id: 'project1', // Root item
-			child_ids: ['item2', 'item3'] // Points to children
-		});
+	// if (!existingProject.length) {
+	// 	// Create nodes
+	// 	await database.nodes.insert({
+	// 		id: 'item1',
+	// 		node_type: 'panel',
+	// 		panel_data: {
+	// 			name: 'Main Panel',
+	// 			circuit_number: 1,
+	// 			ambient_temperature: '25C',
+	// 			phase: 'Single'
+	// 		},
+	// 		load_data: undefined,
+	// 		parent_id: 'project1', // Root item
+	// 		child_ids: ['item2', 'item3'] // Points to children
+	// 	});
 
-		await database.items.insert({
-			id: 'item2',
-			node_type: 'load',
-			panel_data: undefined,
-			load_data: {
-				load_description: 'Light Load',
-				quantity: 10,
-				varies: 0,
-				continuous: 1,
-				special: 'N/A'
-			},
-			parent_id: 'item1',
-			child_ids: ['item4'] // Points to child
-		});
+	// 	await database.nodes.insert({
+	// 		id: 'item2',
+	// 		node_type: 'load',
+	// 		panel_data: undefined,
+	// 		load_data: {
+	// 			load_description: 'Light Load',
+	// 			quantity: 10,
+	// 			varies: 0,
+	// 			continuous: 1,
+	// 			special: 'N/A'
+	// 		},
+	// 		parent_id: 'item1',
+	// 		child_ids: ['item4'] // Points to child
+	// 	});
 
-		await database.items.insert({
-			id: 'item3',
-			node_type: 'panel',
-			panel_data: {
-				name: 'Sub Panel',
-				circuit_number: 2,
-				ambient_temperature: '30C',
-				phase: 'Three'
-			},
-			load_data: undefined,
-			parent_id: 'item1',
-			child_ids: []
-		});
+	// 	await database.nodes.insert({
+	// 		id: 'item3',
+	// 		node_type: 'panel',
+	// 		panel_data: {
+	// 			name: 'Sub Panel',
+	// 			circuit_number: 2,
+	// 			ambient_temperature: '30C',
+	// 			phase: 'Three'
+	// 		},
+	// 		load_data: undefined,
+	// 		parent_id: 'item1',
+	// 		child_ids: []
+	// 	});
 
-		await database.items.insert({
-			id: 'item4',
-			node_type: 'load',
-			panel_data: undefined,
-			load_data: {
-				load_description: 'Fan Load',
-				quantity: 5,
-				varies: 1,
+	// 	await database.nodes.insert({
+	// 		id: 'item12',
+	// 		node_type: 'panel',
+	// 		panel_data: {
+	// 			name: 'Sub sub Panel',
+	// 			circuit_number: 2,
+	// 			ambient_temperature: '30C',
+	// 			phase: 'Three'
+	// 		},
+	// 		load_data: undefined,
+	// 		parent_id: 'item3',
+	// 		child_ids: []
+	// 	});
 
-				continuous: 1,
-				special: 'High Efficiency'
-			},
-			parent_id: 'item2',
-			child_ids: []
-		});
+	// 	await database.nodes.insert({
+	// 		id: 'item13',
+	// 		node_type: 'load',
+	// 		panel_data: undefined,
+	// 		load_data: {
+	// 			load_description: 'Light Loaded',
+	// 			quantity: 10,
+	// 			varies: 0,
+	// 			continuous: 1,
+	// 			special: 'N/A'
+	// 		},
+	// 		parent_id: 'item12',
+	// 		child_ids: []
+	// 	});
 
-		// Create a project
-		const project = await database.projects.insert({
-			id: 'project1',
-			highest_unit_form: {
-				distribution_unit: 'Transformer',
-				wire_length: 60,
-				ambient_temperature: '90',
-				phase: '1PWye'
-			},
-			tree: []
-		});
+	// 	await database.nodes.insert({
+	// 		id: 'item10',
+	// 		node_type: 'load',
+	// 		panel_data: undefined,
+	// 		load_data: {
+	// 			load_description: 'Light Loaded',
+	// 			quantity: 10,
+	// 			varies: 0,
+	// 			continuous: 1,
+	// 			special: 'N/A'
+	// 		},
+	// 		parent_id: 'item3',
+	// 		child_ids: []
+	// 	});
 
-		await project.patch({
-			tree: ['item1']
-		});
-	}
+	// 	await database.nodes.insert({
+	// 		id: 'item11',
+	// 		node_type: 'load',
+	// 		panel_data: undefined,
+	// 		load_data: {
+	// 			load_description: 'Light Loaded',
+	// 			quantity: 10,
+	// 			varies: 0,
+	// 			continuous: 1,
+	// 			special: 'N/A'
+	// 		},
+	// 		parent_id: 'item3',
+	// 		child_ids: []
+	// 	});
 
-	const projs = database.projects.find();
-	const project = (await projs.exec()).at(0);
+	// 	await database.nodes.insert({
+	// 		id: 'item4',
+	// 		node_type: 'load',
+	// 		panel_data: undefined,
+	// 		load_data: {
+	// 			load_description: 'Fan Load',
+	// 			quantity: 5,
+	// 			varies: 1,
+	// 			continuous: 1,
+	// 			special: 'High Efficiency'
+	// 		},
+	// 		parent_id: 'item2',
+	// 		child_ids: []
+	// 	});
 
-	console.log('project', project);
+	// 	// Create a project
+	// 	const project = await database.projects.insert({
+	// 		id: 'project1',
+	// 		highest_unit_form: {
+	// 			distribution_unit: 'Transformer',
+	// 			wire_length: 60,
+	// 			ambient_temperature: '90',
+	// 			phase: '1PWye'
+	// 		},
+	// 		tree: []
+	// 	});
 
-	const items = database.items.find({
-		selector: {
-			parent_id: project?._data.id
-		}
-	});
-	console.log('items', await items.exec());
+	// 	await project.patch({
+	// 		tree: ['item1']
+	// 	});
+	// }
+
+	const project = await getCurrentProject();
+	const nodes = project ? await getChildNodesByParentId(project.id) : [];
+
+	console.log('from layout nodes', nodes);
 
 	return {
 		is_new_file: searchParams.get('new_file') === 'true',
@@ -110,6 +162,7 @@ export const load = async ({ url: { searchParams } }) => {
 		highest_unit_form: await superValidate(zod(highest_unit_schema)),
 		generic_phase_panel_form: await superValidate(zod(generic_phase_panel_schema)),
 		panels: [],
-		// project: 
+		project,
+		nodes
 	};
 };
