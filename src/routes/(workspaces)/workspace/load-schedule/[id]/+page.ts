20294693a21531668dcd6eeac55d45685a7288f6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-06 19:12:52 +0800

    ✨ Implement load function to fetch project and child nodes (loads), enhancing data retrieval in workspace

--- src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
+++ src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
@@ -1,18 +1,26 @@
-import { phase_main_load_schema } from "@/schema/load";
-import { superValidate } from "sveltekit-superforms";
-import { zod } from "sveltekit-superforms/adapters";
+import { phase_main_load_schema } from '@/schema/load';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { getCurrentProject, getChildNodesByParentId } from '@/db/queries/index.js';
+import { goto } from '$app/navigation';
 
 export const entries = () => {
-  // TODO: Fetch entries from db
-  return [
-    { id: 'hello-world' },
-    { id: 'another-blog-post' }
-  ];
+	// TODO: Fetch entries from db
+	return [{ id: 'hello-world' }, { id: 'another-blog-post' }];
 };
+export const load = async ({ params }) => {
+	const project = await getCurrentProject();
 
+	if (!project) {
+		goto('/workspace');
+	}
 
-export const load = (async () => {
-  return {
-    phase_main_load_form: await superValidate(zod(phase_main_load_schema))
-  };
-});
+	const nodes = await getChildNodesByParentId(params.id.split(' ').at(-1) as string);
+	const loads = nodes?.map((node) => node.load_data);
+
+	return {
+		phase_main_load_form: await superValidate(zod(phase_main_load_schema)),
+		project,
+		nodes: loads
+	};
+};
