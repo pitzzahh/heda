674968b4c8196f2e7717225b4397887cbb039c77
Peter John Arao <pitzzahh@gmail.com>
2024-12-07 22:24:42 +0800

    ✨ Add SidebarTree component to sidebar exports

--- src/lib/components/custom/sidebar/index.ts
+++ src/lib/components/custom/sidebar/index.ts
@@ -1,4 +1,5 @@
 export { default as AppSidebar } from './app-sidebar.svelte';
 export { default as SidebarHeader } from './sidebar-header.svelte';
 export { default as SidebarFooter } from './sidebar-footer.svelte';
+export { default as SidebarTree } from './sidebar-tree.svelte';
 export { default as AddPanelAndViewTrigger } from './add-panel-and-view-trigger.svelte';
