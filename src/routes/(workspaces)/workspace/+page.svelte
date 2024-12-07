3356d8854a625dbdaa6c5da41899ba314af42dbb
Peter John Arao <pitzzahh@gmail.com>
2024-12-07 21:53:49 +0800

    🔥 Remove unused imports and TypeScript from workspace page

--- src/routes/(workspaces)/workspace/+page.svelte
+++ src/routes/(workspaces)/workspace/+page.svelte
@@ -1,15 +1,8 @@
-<script lang="ts">
+<script>
 	import { FileQuestionIcon } from '@/assets/icons/lucide';
-	import { buttonVariants } from '$lib/components/ui/button/index.js';
-	import * as Dialog from '$lib/components/ui/dialog/index.js';
-	import { GenericPhaseMainLoadForm } from '@/components/custom/load';
-
-	let { data } = $props();
 </script>
 
 <div class="grid size-full place-content-center place-items-center gap-2">
 	<FileQuestionIcon class="size-24 text-muted-foreground" />
 	<h2 class="text-xl font-semibold text-muted-foreground">No File Selected</h2>
 </div>
-
-
