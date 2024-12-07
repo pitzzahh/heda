e7986592f80393210dc644490d618f9b87251af6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-07 23:09:35 +0800

    ✨ Enhance load schedule page by adding node retrieval and validation checks

--- src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
+++ src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
@@ -1,7 +1,7 @@
 import { phase_main_load_schema } from '@/schema/load';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { getCurrentProject, getChildNodesByParentId } from '@/db/queries/index.js';
+import { getCurrentProject, getChildNodesByParentId, getNodeById } from '@/db/queries/index.js';
 import { goto } from '$app/navigation';
 
 export const entries = () => {
@@ -9,14 +9,20 @@ export const entries = () => {
 	return [{ id: 'hello-world' }, { id: 'another-blog-post' }];
 };
 export const load = async ({ params }) => {
+	const node_id = params.id.split('_').at(-1);
 	const project = await getCurrentProject();
+	const existingNode = await getNodeById(node_id as string);
 
-	if (!project) {
+	if (!project || !existingNode) {
 		goto('/workspace');
 	}
 
-	const nodes = await getChildNodesByParentId(params.id.split('_').at(-1) as string);
-	const loads = nodes?.filter((node) => node.node_type === 'load').map((node) => node.load_data);
+	// MAIIBA PA KANI ANG SHAPE, PETE DAHIL DUMAN SA COMPUTATION.
+	//  PANSAMANTALA MUNA NA LOAD LANG MUNA I RENDER KANG TABLE
+	const nodes = await getChildNodesByParentId(node_id as string);
+	const loads = nodes
+		?.filter((node) => node.node_type === 'load')
+		.map((node) => ({ ...node.load_data, id: node.id }));
 
 	return {
 		phase_main_load_form: await superValidate(zod(phase_main_load_schema)),
