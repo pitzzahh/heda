d819a7f904faec394cefe7c9eb715ed4a9d67b4c
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-08 23:47:15 +0800

    ✨ Update sidebar width style to use dynamic value for improved flexibility

--- src/lib/components/ui/sidebar/sidebar-provider.svelte
+++ src/lib/components/ui/sidebar/sidebar-provider.svelte
@@ -46,7 +46,7 @@
 
 <Tooltip.Provider delayDuration={0}>
 	<div
-		style="--sidebar-width: {SIDEBAR_WIDTH}; --sidebar-width-icon: {SIDEBAR_WIDTH_ICON}; {style}"
+		style="--sidebar-width: {`${sidebar.sidebarWidth}px`}; --sidebar-width-icon: {SIDEBAR_WIDTH_ICON}; {style}"
 		class={cn(
 			'group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar',
 			className
