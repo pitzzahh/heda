cde83b7621268732ba4761e691906fb594e1469e
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-10 23:00:43 +0800

    ♻️ Refactor load function in +page.ts: improve code readability and structure, update load data mapping

--- src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
+++ src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
@@ -1,7 +1,12 @@
 import { phase_main_load_schema } from '@/schema/load';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { getCurrentProject, getChildNodesByParentId, getNodeById, getRootNode } from '@/db/queries/index.js';
+import {
+	getCurrentProject,
+	getChildNodesByParentId,
+	getNodeById,
+	getRootNode
+} from '@/db/queries/index.js';
 import { goto } from '$app/navigation';
 import type { Node } from '@/types/project/index.js';
 
@@ -12,7 +17,7 @@ export const entries = () => {
 export const load = async ({ params }) => {
 	const node_id = params.id.split('_').at(-1) as string;
 	const project = await getCurrentProject();
-	const rootNode = await getRootNode()
+	const rootNode = await getRootNode();
 
 	if (!project || !node_id || !rootNode) {
 		goto('/workspace');
@@ -24,9 +29,12 @@ export const load = async ({ params }) => {
 	// MAIIBA PA KANI ANG SHAPE, PETE DAHIL DUMAN SA COMPUTATION.
 	//  PANSAMANTALA MUNA NA LOAD LANG MUNA I RENDER KANG TABLE
 	const nodes = await getChildNodesByParentId(node_id);
+	// TODO: CREATE A SEPARATE QUERY THAT ALSO GETS THE PANEL WITH ITS COMPUTED VALUES AND CREATE ITS SEPARATE TYPE
 	const loads = nodes
 		?.filter((node) => node.node_type === 'load')
-		.map((node) => ({ ...node.load_data, id: node.id }));
+		.map((node) => ({ ...node, ...node.load_data })) as Node[];
+
+	// NOTE:  bawal ang nested objects digdi. bale magibo kita separate query for computed child panels and loads na maga match sa accesor key kang table
 
 	return {
 		phase_main_load_form: await superValidate(zod(phase_main_load_schema)),
