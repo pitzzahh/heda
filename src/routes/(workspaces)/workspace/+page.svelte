5f75f42a0401a1c7b029baecbb5bbfbe86ec6c26
Peter John Arao <pitzzahh@gmail.com>
2024-12-10 13:14:55 +0800

    💄 Update message for empty state in workspace page: change "No File Selected" to "No Load schedule selected"

--- src/routes/(workspaces)/workspace/+page.svelte
+++ src/routes/(workspaces)/workspace/+page.svelte
@@ -4,5 +4,5 @@
 
 <div class="grid h-[85vh] w-full place-content-center place-items-center gap-2">
 	<FileQuestionIcon class="size-24 text-muted-foreground" />
-	<h2 class="text-xl font-semibold text-muted-foreground">No File Selected</h2>
+	<h2 class="text-xl font-semibold text-muted-foreground">No load schedule selected</h2>
 </div>
