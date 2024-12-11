84f4ca530eaf1b5a8a188959992bf3a8a8e506eb
Peter John Arao <pitzzahh@gmail.com>
2024-12-11 21:20:14 +0800

    ♻️ Set default value for distribution unit in highest unit schema

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { DEFAULT_PHASES_ENUMS } from '@/constants';
-import type { Phase } from '@/types/phase';	
+import type { Phase } from '@/types/phase';
 
 export const highest_unit_schema = z.object({
-	distribution_unit: z.string().refine((v) => v, { message: 'A distribution unit is required.' }),
+	distribution_unit: z.string().refine((v) => v, { message: 'A distribution unit is required.' }).default('Transformer'),
 	phase: z.enum(DEFAULT_PHASES_ENUMS.map((f) => f.value) as [Phase, ...Phase[]], {
 		required_error: 'You need to select a phase'
 	})
