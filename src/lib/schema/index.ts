e2eb265065ac93fe3c72a8a8b134ed4ba41908ad
Peter John Arao <pitzzahh@gmail.com>
2024-12-11 15:40:56 +0800

    🔥Remove unused imports in schema/index

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
-import { MIN_WIRE_LENGTH, DEFAULT_PHASES_ENUMS, DEFAULT_TERMINAL_TEMPERATURE_ENUMS } from '@/constants';
-import type { Temperature } from '@/types/misc';
-import type { Phase } from '@/types/phase';
+import { DEFAULT_PHASES_ENUMS } from '@/constants';
+import type { Phase } from '@/types/phase';	
 
 export const highest_unit_schema = z.object({
 	distribution_unit: z.string().refine((v) => v, { message: 'A distribution unit is required.' }),
