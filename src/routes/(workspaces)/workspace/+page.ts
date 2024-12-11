910ab9bbc418633af10e7aee703b7cc4724f5f06
Peter John Arao <pitzzahh@gmail.com>
2024-12-11 10:01:53 +0000

    ♻️ Add dependency tracking for workspace load function

--- src/routes/(workspaces)/workspace/+page.ts
+++ src/routes/(workspaces)/workspace/+page.ts
@@ -2,7 +2,8 @@ import { phase_main_load_schema } from '@/schema/load';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
-export const load = (async () => {
+export const load = (async ({ depends }) => {
+  depends('app:workspace')
   return {
     phase_main_load_form: await superValidate(zod(phase_main_load_schema))
   };
