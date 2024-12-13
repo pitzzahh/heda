d10636a7e6dd15a60c53edea597211482adf75f7
Peter John Arao <pitzzahh@gmail.com>
2024-12-13 07:59:04 +0000

    ✨ Add getEnumKeys utility function to retrieve keys from enum objects

--- src/lib/utils/enum/index.ts
+++ src/lib/utils/enum/index.ts
@@ -4,6 +4,10 @@ export function getEnumValues<T extends Record<string, string>>(enumObj: T): T[k
   return Object.values(enumObj) as T[keyof T][];
 }
 
+export function getEnumKeys<T extends Record<string, string>>(enumObj: T): (keyof T)[] {
+  return Object.keys(enumObj) as (keyof T)[];
+}
+
 // TODO: May issue, need to fix, unused function
 export function createZodEnum<T extends Record<string, string>>(enumObj: T) {
   return z.enum(Object.values(enumObj) as [T[keyof T], ...T[keyof T][]]);
