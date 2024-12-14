dfd9952aa304f24b9c400ef3e3a0940104ac5408
Peter John Arao <pitzzahh@gmail.com>
2024-12-14 07:32:16 +0000

    🦺 Catch custom varies validation

--- src/lib/schema/load/index.ts
+++ src/lib/schema/load/index.ts
@@ -15,12 +15,27 @@ export const generic_phase_main_load_schema = z.object({
 		message: 'Quantity must be greater than 0.'
 	}),
 	load_description: z.string().refine((v) => v, { message: 'A load description is required.' }),
-	varies: z
-		.string({ message: 'This field is required' })
-		.refine(FIELD_VALIDATION.TEST.ALL_NUMBER, 'This field must be a number'),
+	varies: z.string({ message: 'This field is required' }),
 	continuous: z.boolean(),
 	load_type: z.enum(DEFAULT_LOAD_TYPES_ENUMS.map((f) => f.value) as [LoadType, ...LoadType[]], {
 		errorMap: () => ({ message: 'Please select a valid load type.' })
 	})
+}).superRefine((data, ctx) => {
+	if (data.load_type !== '1P Motor - Rated Horse Power') {
+		if (!data.varies) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				path: ['varies'],
+				message: 'This field is required.'
+			});
+		}
+		if (!FIELD_VALIDATION.TEST.ALL_NUMBER(data.varies)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				path: ['varies'],
+				message: 'This field must be a number.'
+			});
+		}
+	}
 });
 export type GenericPhaseMainLoadSchema = z.infer<typeof generic_phase_main_load_schema>;
