8d17f40f6fce9019876df94cf60bd4657622e121
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-12-14 16:23:56 +0800

    ✨ Add standard ampere ratings and load type z values

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -3,12 +3,11 @@ import { getEnumValues, getEnumKeys } from '@/utils/enum';
 import { toLabelValueArray } from '@/utils/mapper';
 
 export const default_terminal_temperatures = {
-	60: "60",
+	60: '60',
 	75: '75',
-	90: "90",
-	STANDARD_TEMPERATURE: "Standard Temperature"
+	90: '90',
+	STANDARD_TEMPERATURE: 'Standard Temperature'
 } as const;
-
 
 export const DEFAULT_LOADS = [
 	{
@@ -24,67 +23,67 @@ export const DEFAULT_LOADS = [
 		type: 'Lighting Load'
 	},
 	{
-		description: "Small Appliance Load",
+		description: 'Small Appliance Load',
 		varies: 1500,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Laundry Circuit Load",
+		description: 'Laundry Circuit Load',
 		varies: 1500,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Bathroom Branch Circuit",
+		description: 'Bathroom Branch Circuit',
 		varies: 1500,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Garage Branch Circuit",
+		description: 'Garage Branch Circuit',
 		varies: 1500,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Convenience Outlet (Simplex)",
+		description: 'Convenience Outlet (Simplex)',
 		varies: 180,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Convenience Outlet (Duplex)",
+		description: 'Convenience Outlet (Duplex)',
 		varies: 180,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Convenience Outlet (Triplex)",
+		description: 'Convenience Outlet (Triplex)',
 		varies: 180,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "4-Gang Convenience Outlet",
+		description: '4-Gang Convenience Outlet',
 		varies: 360,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "5-Gang Convenience Outlet",
+		description: '5-Gang Convenience Outlet',
 		varies: 450,
 		continuous: true,
 		type: 'Convenience Outlet'
 	},
 	{
-		description: "Dwelling Unit",
+		description: 'Dwelling Unit',
 		varies: 24,
 		continuous: true,
 		type: 'General Lighting'
 	},
 	{
-		description: "Office Buildings",
+		description: 'Office Buildings',
 		varies: 28,
 		continuous: true,
 		type: 'General Lighting'
@@ -104,14 +103,13 @@ export const default_phases = {
 } as const;
 
 export const default_load_types = {
-	LIGHTING_LOAD: "Lighting Load",
-	CONVENIENCE_OUTLET: "Convenience Outlet",
-	GENERAL_LIGHTING: "General Lighting",
-	ONE_P_MOTOR__RATED_HORSE_POWER: "1P Motor - Rated Horse Power",
-	ONE_P_MOTOR__RATED_CURRENT: "1P Motor - Rated Current",
-	HEATING_EQUIPMENT: "Heating Equipment",
+	LIGHTING_LOAD: 'Lighting Load',
+	CONVENIENCE_OUTLET: 'Convenience Outlet',
+	GENERAL_LIGHTING: 'General Lighting',
+	ONE_P_MOTOR__RATED_HORSE_POWER: '1P Motor - Rated Horse Power',
+	ONE_P_MOTOR__RATED_CURRENT: '1P Motor - Rated Current',
+	HEATING_EQUIPMENT: 'Heating Equipment'
 } as const;
-
 
 export const load_type_to_varies_label = {
 	[default_load_types.LIGHTING_LOAD]: 'Unit Wattage',
@@ -121,6 +119,20 @@ export const load_type_to_varies_label = {
 	[default_load_types.ONE_P_MOTOR__RATED_CURRENT]: 'Current Rating',
 	[default_load_types.HEATING_EQUIPMENT]: 'Unit Wattage'
 } as const satisfies Record<LoadType, VariesLabel>;
+
+export const standard_ampere_ratings = [
+	15, 20, 30, 40, 50, 60, 70, 80, 100, 125, 150, 175, 200, 225, 250, 300, 350, 400, 500, 600, 800,
+	1000, 1200, 1600, 2000, 2500, 3000, 4000, 5000, 6000
+] as const;
+
+export const load_type_z_value = {
+	[default_load_types.LIGHTING_LOAD]: 1.25,
+	[default_load_types.CONVENIENCE_OUTLET]: 1.25,
+	[default_load_types.GENERAL_LIGHTING]: 1.25,
+	[default_load_types.ONE_P_MOTOR__RATED_HORSE_POWER]: 2.5,
+	[default_load_types.ONE_P_MOTOR__RATED_CURRENT]: 2.5,
+	[default_load_types.HEATING_EQUIPMENT]: 1.5
+} as const satisfies Record<LoadType, number>;
 
 export const default_hp_current_relationship = {
 	'1/6': '2.2',
@@ -137,7 +149,9 @@ export const default_hp_current_relationship = {
 	'10': '50'
 } as const;
 
-export const DEFAULT_HP_CURRENT_RELATIONSHIP_ARRAY = Object.entries(default_hp_current_relationship).map(([hp, current]) => ({ hp, current }));
+export const DEFAULT_HP_CURRENT_RELATIONSHIP_ARRAY = Object.entries(
+	default_hp_current_relationship
+).map(([hp, current]) => ({ hp, current }));
 
 export const DEFAULT_THREE_PHASE_TYPES_ENUMS = toLabelValueArray(default_three_phase_types);
 
