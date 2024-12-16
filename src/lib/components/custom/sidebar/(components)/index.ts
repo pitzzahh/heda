d134fe5336f62ab40117b8c5934473e5929d1060
Peter John Arao <pitzzahh@gmail.com>
2024-12-16 05:23:08 +0000

    ✨ Add sidebar tree icons component for enhanced UI elements

---
+++ src/lib/components/custom/sidebar/(components)/index.ts
@@ -0,0 +1,1 @@
+export * from './sidebar-tree-icons.svelte';
---
+++ src/lib/components/custom/sidebar/(components)/sidebar-tree-icons.svelte
@@ -0,0 +1,25 @@
+<script module>
+	import { Pencil, CirclePlus, Grid2x2Plus, Trash2, Copy } from '@/assets/icons';
+
+	export { Grid2x2PlusIcon, CirclePlusIcon, PencilIcon, CopyIcon, Trash2Icon };
+</script>
+
+{#snippet Grid2x2PlusIcon(/** @type {string} */ className)}
+	<Grid2x2Plus class={className} />
+{/snippet}
+
+{#snippet CirclePlusIcon(/** @type {string} */ className)}
+	<CirclePlus class={className} />
+{/snippet}
+
+{#snippet PencilIcon(/** @type {string} */ className)}
+	<Pencil class={className} />
+{/snippet}
+
+{#snippet CopyIcon(/** @type {string} */ className)}
+	<Copy class={className} />
+{/snippet}
+
+{#snippet Trash2Icon(/** @type {string} */ className)}
+	<Trash2 class={className} />
+{/snippet}
