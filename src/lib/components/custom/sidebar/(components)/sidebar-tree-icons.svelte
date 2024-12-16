af6b08eccfb7fd39f2e5532c6077d917efa247ee
Peter John Arao <pitzzahh@gmail.com>
2024-12-16 05:38:24 +0000

    ✨ Update icon component snippets to allow undefined className type

--- src/lib/components/custom/sidebar/(components)/sidebar-tree-icons.svelte
+++ src/lib/components/custom/sidebar/(components)/sidebar-tree-icons.svelte
@@ -4,22 +4,22 @@
 	export { Grid2x2PlusIcon, CirclePlusIcon, PencilIcon, CopyIcon, Trash2Icon };
 </script>
 
-{#snippet Grid2x2PlusIcon(/** @type {string} */ className)}
+{#snippet Grid2x2PlusIcon(/** @type {string | undefined} */ className)}
 	<Grid2x2Plus class={className} />
 {/snippet}
 
-{#snippet CirclePlusIcon(/** @type {string} */ className)}
+{#snippet CirclePlusIcon(/** @type {string | undefined} */ className)}
 	<CirclePlus class={className} />
 {/snippet}
 
-{#snippet PencilIcon(/** @type {string} */ className)}
+{#snippet PencilIcon(/** @type {string | undefined} */ className)}
 	<Pencil class={className} />
 {/snippet}
 
-{#snippet CopyIcon(/** @type {string} */ className)}
+{#snippet CopyIcon(/** @type {string | undefined} */ className)}
 	<Copy class={className} />
 {/snippet}
 
-{#snippet Trash2Icon(/** @type {string} */ className)}
+{#snippet Trash2Icon(/** @type {string | undefined} */ className)}
 	<Trash2 class={className} />
 {/snippet}
