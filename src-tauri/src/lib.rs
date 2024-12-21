2678d4bc50bab2ed9edc861d54e03f7c41ad42ac
Peter John Arao <pitzzahh@gmail.com>
2024-12-22 01:17:12 +0800

    🔥 Remove outdated update handling logic from the Tauri application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,40 +1,7 @@
-use tauri_plugin_updater::UpdaterExt;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-            update(handle).await.unwrap();
-            });
-            Ok(())
-        })
         .plugin(tauri_plugin_log::Builder::new().build())
         .run(tauri::generate_context!())
-        .unwrap()
         .expect("error while running tauri application");
 }
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app.updater()?.check().await? {
-      let mut downloaded = 0;
-      // alternatively we could also call update.download() and update.install() separately
-      update
-        .download_and_install(
-          |chunk_length, content_length| {
-            downloaded += chunk_length;
-            println!("downloaded {downloaded} from {content_length:?}");
-          },
-          || {
-            println!("download finished");
-          },
-        )
-        .await?;
-  
-      println!("update installed");
-      app.restart();
-    }
-  
-    Ok(())
-  }
