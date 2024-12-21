3d2207c8fb73e99fa36ef1678006f8be12abfc47
Alessandro Benig <alebenig4@gmail.com>
2024-12-21 22:59:39 +0800

    ♻️ Add computeConductorSize function and related utilities for conductor sizing calculations

--- src/lib/utils/computations/index.ts
+++ src/lib/utils/computations/index.ts
@@ -1,7 +1,21 @@
-import type { DEFAULT_LOAD_TYPES_OPTIONS } from '@/constants';
-import { standard_ampere_ratings, load_type_z_value } from '@/constants';
+import {
+	standard_ampere_ratings,
+	load_type_z_value,
+	AMPACITY_RANGES,
+	AMPACITY_TO_CONDUCTOR_SIZE
+} from '@/constants';
+import type { LoadType } from '@/types/load';
 
-type LoadType = (typeof DEFAULT_LOAD_TYPES_OPTIONS)[number];
+// load types with the same formula
+const commonLoadTypes = [
+	'Lighting Load',
+	'Convenience Outlet',
+	'General Lighting',
+	'Heating Equipment'
+] as const;
+
+type CommonLoadTypes = (typeof commonLoadTypes)[number];
+
 export function computeVoltAmpere({
 	load_type,
 	quantity,
@@ -13,16 +27,8 @@ export function computeVoltAmpere({
 }) {
 	const is_varies_number = typeof varies === 'number';
 
-	// load types with the same formula
-	const commonLoadTypes = [
-		'Lighting Load',
-		'Convenience Outlet',
-		'General Lighting',
-		'Heating Equipment'
-	] as const;
-
 	if (is_varies_number) {
-		if (commonLoadTypes.includes(load_type as (typeof commonLoadTypes)[number])) {
+		if (commonLoadTypes.includes(load_type as CommonLoadTypes)) {
 			return quantity * varies;
 		}
 
@@ -55,4 +61,66 @@ export function computeAmpereTrip(current: number, load_type?: LoadType) {
 	}
 
 	return 0;
+}
+
+export function computeConductorSize({
+	set,
+	// ambient_temp = 30, //pansamantala
+	qty,
+	load_type,
+	at,
+	current
+}: {
+	set: number;
+	qty: number;
+	ambient_temp?: number;
+	load_type: LoadType | 'Main';
+	at: number;
+	current: number;
+}) {
+	const load_type_parameter = getLoadTypeParams(load_type as CommonLoadTypes);
+	const total_num_of_conductors = set * qty;
+
+	// digdi ang usage kang ambient temp, but 30 muna and 1 pansamantagal
+	const correction_factor = 1; // TODO: create an array of temp rating
+	const adjustment_factor = getAdjustmentFactor(total_num_of_conductors);
+
+	const load_type_output =
+		load_type === 'Main' ? 1.25 : load_type_parameter === 'at' ? at : 1.25 * current;
+	const adjusted_current = load_type_output / (correction_factor * adjustment_factor * set);
+
+	const range = AMPACITY_RANGES.find(
+		(range) => adjusted_current > range.min && adjusted_current < range.max
+	);
+	const final_ampacity = range ? range.value : AMPACITY_RANGES[0].value;
+
+	return AMPACITY_TO_CONDUCTOR_SIZE[final_ampacity];
+}
+
+function getLoadTypeParams(load_type: LoadType): 'at' | 'multiply-current' {
+	if (commonLoadTypes.includes(load_type as CommonLoadTypes)) {
+		return 'at';
+	}
+
+	return 'multiply-current';
+}
+
+function getAdjustmentFactor(numberOfConductors: number): number {
+	if (numberOfConductors <= 3) {
+		return 1.0;
+	}
+
+	if (numberOfConductors <= 6) {
+		return 0.8;
+	} else if (numberOfConductors <= 9) {
+		return 0.7;
+	} else if (numberOfConductors <= 20) {
+		return 0.5;
+	} else if (numberOfConductors <= 30) {
+		return 0.45;
+	} else if (numberOfConductors <= 40) {
+		return 0.4;
+	} else {
+		return 0.35;
+	}
 }
