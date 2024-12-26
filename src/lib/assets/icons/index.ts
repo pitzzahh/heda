1ae82ceef2521ffeaa5957cdb334a9d1e0ad4bcc
Peter John Arao <pitzzahh@gmail.com>
2024-12-26 23:29:52 +0800

    🍱 Fix icon export name for ChevronDown in the icon library

--- src/lib/assets/icons/index.ts
+++ src/lib/assets/icons/index.ts
@@ -25,4 +25,4 @@ export { default as Copy } from 'lucide-svelte/icons/copy';
 export { default as Loader } from 'lucide-svelte/icons/loader';
 export { default as X } from 'lucide-svelte/icons/x';
 export { default as ChevronUp } from 'lucide-svelte/icons/chevron-up';
-export { default as ChevronUDown} from 'lucide-svelte/icons/chevron-down';
+export { default as ChevronDown } from 'lucide-svelte/icons/chevron-down';
