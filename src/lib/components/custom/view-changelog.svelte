38b48863128652b88fa79144e17a9afbaef2fae2
Peter John Arao <pitzzahh@gmail.com>
2024-12-27 07:24:09 +0800

    ✨ Replace Drawer component with Sheet component in ViewChangelog

--- src/lib/components/custom/view-changelog.svelte
+++ src/lib/components/custom/view-changelog.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
 	import DOMPurify from 'dompurify';
+	import Markdown from 'svelte-exmarkdown';
 	import { marked } from 'marked';
 	import { buttonVariants } from '@/components/ui/button';
-	import * as Drawer from '@/components/ui/drawer/index.js';
+	import * as Sheet from '@/components/ui/sheet/index.js';
+	import { ScrollArea } from '@/components/ui/scroll-area/index.js';
 	import { History } from '@/assets/icons';
 </script>
 
-<Drawer.Root>
-	<Drawer.Trigger class={buttonVariants({ variant: 'ghost', size: 'sm' })}>
+<Sheet.Root>
+	<Sheet.Trigger class={buttonVariants({ variant: 'ghost', size: 'sm' })}>
 		<History />
 		View Changelog
-	</Drawer.Trigger>
-	<Drawer.Content class="max-h-96">
-		<Drawer.Header>
-			<Drawer.Title>Changelog</Drawer.Title>
-			<Drawer.Description>This contains the changelog</Drawer.Description>
-		</Drawer.Header>
+	</Sheet.Trigger>
+	<Sheet.Content class="min-w-[55%]">
+		<Sheet.Header>
+			<Sheet.Title>Changelog</Sheet.Title>
+			<Sheet.Description>This contains the changelog</Sheet.Description>
+		</Sheet.Header>
 
 		{#await fetch('https://raw.githubusercontent.com/pitzzahh/heda/refs/heads/main/changelog.md')}
 			<p>Loading...</p>
@@ -23,20 +25,14 @@
 			{#await raw_changelog.text()}
 				<p>Loading...</p>
 			{:then response_body}
-				{#await marked(response_body)}
-					<p>Loading...</p>
-				{:then changelog}
-					<div class="p-2">
-						{@html DOMPurify.sanitize(changelog)}
-					</div>
-				{:catch error}
-					<p>{error.message}</p>
-				{/await}
+				<ScrollArea class="mt-2 h-[90%]">
+					<Markdown md={DOMPurify.sanitize(response_body)} />
+				</ScrollArea>
 			{:catch error}
 				<p>{error.message}</p>
 			{/await}
 		{:catch error}
 			<p>{error.message}</p>
 		{/await}
-	</Drawer.Content>
-</Drawer.Root>
+	</Sheet.Content>
+</Sheet.Root>
