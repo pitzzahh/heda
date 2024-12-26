2f211fac18c692d5597cdbed96b50ee4065e7d2b
Peter John Arao <pitzzahh@gmail.com>
2024-12-27 00:05:12 +0800

    ♻️ Enhance settings dialog layout and add app version display

--- src/lib/components/custom/settings-dialog.svelte
+++ src/lib/components/custom/settings-dialog.svelte
@@ -64,23 +64,119 @@
 				<Dialog.Content class="sm:max-w-[425px]">
 					<Dialog.Header>
 						<Dialog.Title class="text-center">Settings</Dialog.Title>
-						<Dialog.Description class="text-center">Set your preferences in the application</Dialog.Description>
+						<Dialog.Description class="text-center"
+							>Set your preferences in the application</Dialog.Description
+						>
 					</Dialog.Header>
-						<div class="flex flex-col gap-2">
-							<p class="font-semibold">Project</p>
-							<div class="flex flex-col gap-3">
-								<Label for="adjustment_factor">Adjustment Factor</Label>
-								<div class="flex w-full justify-between">
-									<Switch
-										disabled={!!!project}
-										id="adjustment_factor"
-										bind:checked={is_adjustment_factor_constant}
-									/>
+					<div class="flex flex-col gap-2">
+						<p class="font-semibold">Project</p>
+						<div class="flex flex-col gap-3">
+							<Label for="adjustment_factor">Adjustment Factor</Label>
+							<div class="flex w-full justify-between">
+								<Switch
+									disabled={!!!project}
+									id="adjustment_factor"
+									bind:checked={is_adjustment_factor_constant}
+								/>
 
-									{#if has_changes}
-										<Button size="sm" onclick={handleSaveChanges}>Save Changes</Button>
+								{#if has_changes}
+									<Button size="sm" onclick={handleSaveChanges}>Save Changes</Button>
+								{/if}
+							</div>
+
+							<p class="text-xs text-muted-foreground">
+								{is_adjustment_factor_constant
+									? 'The adjustment factor for all loads will be set to 100%.'
+									: 'The adjustment factor for each load may vary between 100%, 80%, 70%, 50%, 45%, 40%, and 35%, depending on the total number of conductors.'}
+							</p>
+						</div>
+					</div>
+					<Separator class="my-1 w-full" />
+					<div class="flex flex-col gap-3">
+						<p class="font-semibold">Preferences</p>
+						<div class="flex flex-col gap-3">
+							<Label for="colors">Theme Color</Label>
+							<div id="colors" class="flex items-center gap-2">
+								{#each themeColors as themeColor, index (index)}
+									<button
+										aria-label="color"
+										class={cn(themeColor.bg, 'size-8 rounded-full', {
+											'outline outline-2 outline-offset-1 outline-blue-500':
+												themeColor.value === settingsState.themeColor
+										})}
+										onclick={() => handleChangeThemeColor(themeColor.value)}
+									></button>
+								{/each}
+							</div>
+						</div>
+
+						<div class="flex flex-col gap-3">
+							<Label for="font-trigger">Font</Label>
+							<Select.Root
+								type="single"
+								value={selectedFont}
+								onValueChange={(value) => settingsState.setFont(value as Font)}
+							>
+								<Select.Trigger class="w-[180px]" id="font-trigger" placeholder="Select a font">
+									{selectedFont.charAt(0).toUpperCase().concat(selectedFont.slice(1))}
+								</Select.Trigger>
+								<Select.Content>
+									{#each ['default', 'isocpeur', 'verdana'] as item, index (index)}
+										<Select.Item class={item} disabled={selectedFont === item} value={item}
+											>{item.charAt(0).toUpperCase().concat(item.slice(1))}</Select.Item
+										>
+									{/each}
+								</Select.Content>
+							</Select.Root>
+						</div>
+					</div>
+					<Separator class="my-1 w-full" />
+					<div class="flex flex-col gap-2">
+						<p class="font-semibold">App Version</p>
+						<span>v{pj.version}</span>
+						<svelte:boundary>
+							<Button
+								onclick={async () => {
+									update_state = 'processing';
+									try {
+										app_update = await checkForUpdates();
+										console.log('app_update', app_update);
+										update_state = app_update ? 'available' : 'no_updates';
+									} catch (error) {
+										update_state = 'no_updates';
+										toast.info('No updates available');
+									}
+								}}
+							>
+								<Loader
+									class={cn('mr-1 hidden h-4 w-4 animate-spin', {
+										block: update_state === 'processing'
+									})}
+								/>
+								<span>
+									{#if update_state === 'processing'}
+										Checking
+									{:else if update_state === 'available'}
+										v{app_update?.version}
+									{:else if update_state === 'no_updates'}
+										No updates available
+									{:else}
+										Check for updates
 									{/if}
-								</div>
+								</span>
+							</Button>
+							{#snippet failed(error, reset)}
+								<p class="text-sm text-muted-foreground">{error}</p>
+								<Button onclick={reset}>oops! try again</Button>
+							{/snippet}
+						</svelte:boundary>
+					</div>
+				</Dialog.Content>
+			</Dialog.Root>
+		</Tooltip.Trigger>
+		<Tooltip.Content>Settings</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
 
 								<p class="text-xs text-muted-foreground">
 									{is_adjustment_factor_constant
