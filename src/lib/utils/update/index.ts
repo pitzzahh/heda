b20176c3869c2d6a7e236e0f55448bebe3e5826e
Peter John Arao <pitzzahh@gmail.com>
2024-12-26 21:22:31 +0800

    ✨ Add toast notifications for update checks and installation progress

--- src/lib/utils/update/index.ts
+++ src/lib/utils/update/index.ts
@@ -1,11 +1,13 @@
 import { check, Update } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
+import { toast } from 'svelte-sonner';
 
 /**
  * A function that checks for updates and installs them if the provided function returns true.
  * @param fn A function that returns a boolean value. If the function returns true, the update will be installed.
  */
 export async function checkForUpdates() {
+  toast.loading('Checking for updates...');
   return await check();
 }
 
@@ -13,6 +15,7 @@ export async function installUpdate(update: Update, fn: (fn: () => Promise<void>
   console.log(
     `found update ${update.version} from ${update.date} with notes ${update.body}`
   );
+  toast.info(`found update ${update.version} from ${update.date} with notes ${update.body}`);
   let downloaded = 0;
   let contentLength = 0;
   // alternatively we could also call update.download() and update.install() separately
@@ -21,6 +24,7 @@ export async function installUpdate(update: Update, fn: (fn: () => Promise<void>
       case 'Started':
         contentLength = event.data.contentLength ?? 0;
         console.log(`started downloading ${contentLength} bytes`);
+        toast.info(`started downloading ${contentLength} bytes`);
         break;
       case 'Progress':
         downloaded += event.data.chunkLength;
