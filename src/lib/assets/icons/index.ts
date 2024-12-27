d9ea3cc9e3f318a1fd25586f818e4f9e8fa1b779
Peter John Arao <pitzzahh@gmail.com>
2024-12-27 21:04:50 +0800

    🍱 Add PackageCheck icon to icon exports

--- src/lib/assets/icons/index.ts
+++ src/lib/assets/icons/index.ts
@@ -28,3 +28,4 @@ export { default as ChevronUp } from 'lucide-svelte/icons/chevron-up';
 export { default as ChevronDown } from 'lucide-svelte/icons/chevron-down';
 export { default as FileClock } from 'lucide-svelte/icons/file-clock';
 export { default as History } from 'lucide-svelte/icons/history';
+export { default as PackageCheck } from 'lucide-svelte/icons/package-check';
