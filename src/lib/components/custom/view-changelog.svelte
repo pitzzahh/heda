114366823b4cff5eae2ed3b48eabb773476d3705
Peter John Arao <pitzzahh@gmail.com>
2024-12-27 21:20:41 +0800

    ♻️ Refactor changelog component to use Svelte Markdown for rendering

--- src/lib/components/custom/view-changelog.svelte
+++ src/lib/components/custom/view-changelog.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import DOMPurify from 'dompurify';
 	import Markdown from 'svelte-exmarkdown';
-	import { marked } from 'marked';
+	// import { marked } from 'marked';
 	import { buttonVariants } from '@/components/ui/button';
 	import * as Sheet from '@/components/ui/sheet/index.js';
 	import { ScrollArea } from '@/components/ui/scroll-area/index.js';
@@ -26,7 +26,8 @@
 				<p>Loading...</p>
 			{:then response_body}
 				<ScrollArea class="mt-2 h-[90%]">
-					{@html marked(DOMPurify.sanitize(response_body))}
+					<!-- {@html marked(DOMPurify.sanitize(response_body))} -->
+					<Markdown md={DOMPurify.sanitize(response_body)} />
 				</ScrollArea>
 			{:catch error}
 				<p>{error.message}</p>
