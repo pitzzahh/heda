59ab46873b2dfb84b4e905a87a0b0f2da5fcb424
Peter John Arao <pitzzahh@gmail.com>
2024-12-29 21:09:09 +0800

    ✨ Add functions to format feeder conductor and conduit with validation checks

--- src/lib/utils/format/index.ts
+++ src/lib/utils/format/index.ts
@@ -29,3 +29,31 @@ export function getOrdinalSuffix(n: number): string {
   const v = n % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
+
+/**
+ * Returns a formatted feeder conductor.
+ * @param conductor_size The size of the feeder conductor
+ * @param conductor_type The type of the feeder conductor
+ * @param validate A flag used to perform validaton of inputs or not
+ * @returns a formatted feeder conductor.
+ */
+export function getFeederConductor(conductor_size: number, conductor_type: string, validate: boolean = false) {
+  if (validate && (conductor_size === 0 || conductor_type === '')) {
+    return `Conductor size and type are required.`
+  }
+  return conductor_size.toString().concat(' ', conductor_type);
+}
+
+/**
+ * Returns a formatted conduit.
+ * @param conduit_size The size of the conduit
+ * @param conduit_type The type of the conduit
+ * @param validate A flag used to perform validaton of inputs or not
+ * @returns a formatted conduit.
+ */
+export function getConduit(conduit_size: number, conduit_type: string, validate: boolean = false) {
+  if (validate && (conduit_size === 0 || conduit_type === '')) {
+    return `Conduit size and type are required.`
+  }
+  return conduit_size.toString().concat(' ', conduit_type);
+}
