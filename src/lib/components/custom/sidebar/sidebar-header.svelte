b6dd561aee6a15e4f7844b0bf64d79e7fb8b1381
Peter John Arao <pitzzahh@gmail.com>
2024-12-29 22:16:27 +0800

    💡 commented-out code for l(t) calculation in sidebar footer

--- src/lib/components/custom/sidebar/sidebar-header.svelte
+++ src/lib/components/custom/sidebar/sidebar-header.svelte
@@ -389,13 +389,13 @@
 					main_columns.forEach(({ column, value }) => set_main_cell(column, value));
 
 					// bottom part
-					const last_cell = worksheet.getCell(`A${last_row + 1}`);
-					last_cell.value =
-						`l(t) = 1.25 * ${node_data_summary.voltage} `;
-						last_cell.alignment = centerAlignment;
-					const last_total_cell = worksheet.getCell(`C${last_row + 1}`)
-					last_total_cell.value = 1.25 * node_data_summary.voltage;
-					last_total_cell.alignment = centerAlignment;
+					// const last_cell = worksheet.getCell(`A${last_row + 1}`);
+					// last_cell.value =
+					// 	`l(t) = 1.25 * ${node_data_summary.voltage} `;
+					// 	last_cell.alignment = centerAlignment;
+					// const last_total_cell = worksheet.getCell(`C${last_row + 1}`)
+					// last_total_cell.value = 1.25 * node_data_summary.voltage;
+					// last_total_cell.alignment = centerAlignment;
 
 					end_row += last_row + 3;
 					await processNodeChildren(child.id, child, depth + 1, last_row);
