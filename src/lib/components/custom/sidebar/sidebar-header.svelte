91479de3abd8ffc5d9dab093a6a70ed13e5a20fe
Peter John Arao <pitzzahh@gmail.com>
2025-01-03 16:13:40 +0800

    ✨ Refactor exportToExcel function to improve parameter handling and streamline export process

--- src/lib/components/custom/sidebar/sidebar-header.svelte
+++ src/lib/components/custom/sidebar/sidebar-header.svelte
@@ -9,13 +9,10 @@
 	import type { Project, Node } from '@/db/schema';
 	import { dev } from '$app/environment';
 	import UndoRedoButtons from './(components)/undo-redo-buttons.svelte';
-	import { processOnePhaseExcelPanelBoardSchedule } from '@/helpers/export';
+	import { exportToExcel, processOnePhaseExcelPanelBoardSchedule } from '@/helpers/export';
+	import type { ButtonState } from '@/types/misc';
 
 	let { project, root_node }: { project?: Project; root_node: Node } = $props();
-
-	const settingsState = getSettingsState();
-
-	type ButtonState = 'idle' | 'loading';
 
 	let button_states = $state({
 		export_to_excel: 'idle' as ButtonState
@@ -24,103 +21,6 @@
 	function handleSave() {
 		// TODO: Implement save functionality
 		toast.warning('This feature is not yet implemented');
-	}
-
-	async function exportToExcel() {
-		dev && console.log('Root:', root_node);
-		if (!root_node) {
-			return toast.warning('No project found', {
-				description: 'Cannot proceed with the export.',
-				position: 'bottom-center'
-			});
-		}
-
-		const highest_unit = root_node?.highest_unit_form;
-		if (!highest_unit) {
-			return toast.warning('No highest unit found, nothing to export', {
-				description: 'This is a system error and should not be here, the error has been logged.',
-				position: 'bottom-center'
-			});
-		}
-		const file_name = project?.project_name ?? 'Exported Panelboard Schedule';
-		button_states.export_to_excel = 'loading';
-		toast.info(`Exporting to Excel... ${file_name}.xlsx`, {
-			description: 'Please wait, this should last very long.',
-			position: 'bottom-center'
-		});
-
-		const workbook = new ExcelJS.Workbook();
-		workbook.title = 'Exported Panelboard Schedule';
-		workbook.creator = 'HEDA(Desktop App)';
-
-		try {
-			switch (highest_unit?.phase) {
-				case '1P':
-					workbook.subject = '1P Load Schedule';
-					workbook.category = ['1P', 'Load Schedule', 'Export'].join(',');
-					workbook.description = 'Load schedule for 1 phase load schedule';
-					const process_result = await processOnePhaseExcelPanelBoardSchedule(
-						workbook,
-						root_node.id,
-						highest_unit
-					);
-					if (!process_result.valid) {
-						button_states.export_to_excel = 'idle';
-						return toast.warning(process_result.message ?? 'Something went wrong while exporting', {
-							description: process_result?.is_system_error
-								? 'This is a system error and should not be here, the error has been logged.'
-								: (process_result?.description ?? undefined),
-							position: 'bottom-center'
-						});
-					}
-					break;
-				case '3P':
-					workbook.subject = '3P Load Schedule';
-					workbook.category = ['3P', 'Load Schedule', 'Export'].join(',');
-					workbook.description = 'Load schedule for 3 phase load schedule';
-					return toast.warning('This feature is still under development', {
-						description: 'Three phase load schedule is not yet supported',
-						position: 'bottom-center'
-					});
-				default:
-					button_states.export_to_excel = 'idle';
-					workbook.subject = 'Unknown Load Schedule';
-					return toast.warning('Something went wrong while exporting', {
-						description:
-							'This is a system error and should not be here, the error has been logged.',
-						position: 'bottom-center'
-					});
-			}
-		} catch (e) {
-			button_states.export_to_excel = 'idle';
-			return toast.warning(`Something went wrong while exporting:: ${e?.toString()}`, {
-				description: 'This is a system error and should not be here, the error has been logged.',
-				position: 'bottom-center'
-			});
-		}
-
-		// Write the workbook and trigger download
-		const buffer = await workbook.xlsx.writeBuffer();
-		const blob = new Blob([buffer], {
-			type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-		});
-		const url = URL.createObjectURL(blob);
-
-		// Create a link and download the file
-		const link = document.createElement('a');
-		link.href = url;
-		link.download = `${file_name}.xlsx`;
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-
-		// Free resources
-		URL.revokeObjectURL(url);
-		toast.success('Export finished successfully.', {
-			description: 'The file has been downloaded successfully',
-			position: 'bottom-center'
-		});
-		button_states.export_to_excel = 'idle';
 	}
 </script>
 
@@ -147,7 +47,15 @@
 				<Tooltip.Trigger
 					disabled={button_states.export_to_excel === 'loading'}
 					class={buttonVariants({ variant: 'outline', size: 'sm' })}
-					onclick={exportToExcel}
+					onclick={() =>
+						exportToExcel(
+							root_node.id,
+							root_node?.highest_unit_form,
+							undefined,
+							project?.project_name,
+							() => (button_states.export_to_excel = 'idle'),
+							() => (button_states.export_to_excel = 'loading')
+						)}
 				>
 					<FileUp class="h-4 w-4" />
 					Export to Excel
