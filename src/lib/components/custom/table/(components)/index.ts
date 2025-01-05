15b9934997ae78add09a251161bc5781c04f44b2
Alessandro Benig <alebenig4@gmail.com>
2025-01-05 13:45:01 +0800

    ✨ Refactor column imports and update to use ActionsColumnDropdown for improved component structure

--- src/lib/components/custom/table/(components)/index.ts
+++ src/lib/components/custom/table/(components)/index.ts
@@ -1,1 +1,8 @@
-export { default as ColumnDropdown } from './column-dropdown.svelte';
+export { default as ActionsColumnDropdown } from './actions-column-dropdown.svelte';
+export { default as AtFooterCell } from './at-footer-cell.svelte';
+export { default as ConductorSetsCell } from './conductor-sets-cell.svelte';
+export { default as ErrorCell } from './error-cell.svelte';
+export { default as InsulationsDropdown } from './insulations-dropdown.svelte';
+export { default as LoadDescriptionCell } from './load-description-cell.svelte';
+export { default as PoleDropdown } from './pole-dropdown.svelte';
+export { default as VdCurrentCell } from './vd-current-cell.svelte';
