33bc119b3d8ea1715b73acb95dc369280ed97872
Peter John Arao <pitzzahh@gmail.com>
2025-01-06 22:22:38 +0800

    ✨ Implement project creation with encrypted backup file functionality

--- src/lib/components/custom/highest-unit-form.svelte
+++ src/lib/components/custom/highest-unit-form.svelte
@@ -12,6 +12,12 @@
 	import { DEFAULT_PHASES_OPTIONS } from '@/constants';
 	import { createProject } from '@/db/mutations/index';
 	import type { Project, Node } from '@/db/schema';
+	import { BaseDirectory, create } from '@tauri-apps/plugin-fs';
+	import { getChildNodesByParentId } from '@/db/queries';
+	import { getEnv, writeEncryptedFile } from '@/helpers/security';
+	import type { FileExport } from '@/types/main';
+
+	const secretKey = 'your-strong-secret-key';
 
 	interface Props {
 		highest_unit_form: T;
@@ -32,10 +38,30 @@
 				};
 				await invalidate('app:workspace');
 
-				toast.success('Form is valid');
+				toast.success('Project created successfully');
 				const redirect_url = `/workspace/load-schedule/${form.data.distribution_unit}_${created_proj.root_node_id}`;
 				goto(redirect_url);
 				closeDialog();
+				const nodes = await getChildNodesByParentId(created_proj.root_node_id);
+				const backup: FileExport = { project: created_proj.project, nodes };
+
+				try {
+					const secret_key = await getEnv('APP_SECRET_KEY');
+					if (!secret_key) {
+						return toast.warning('Failed to create new file', {
+							description:
+								'This is a system error and should not be here, the error has been logged.'
+						});
+					}
+
+					await writeEncryptedFile(
+						created_proj.project?.project_name ?? 'Untitled',
+						backup,
+						secret_key
+					);
+				} catch (err) {
+					console.error(err);
+				}
 			} else {
 				toast.error('Form is invalid');
 			}
