0410287a991572b672f0561b51c42714f3bd8826
Peter John Arao <pitzzahh@gmail.com>
2025-01-06 21:37:56 +0800

    ✨ Add cleanup and leader election plugins to database initialization; update memory storage default to true

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -8,6 +8,8 @@ import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import type { MyDatabaseCollections } from '@/types/db';
 import { project_schema, node_schema } from '@/db/schema/index.js';
 import { RxDBUpdatePlugin } from 'rxdb/plugins/update';
+import { RxDBCleanupPlugin } from 'rxdb/plugins/cleanup';
+import { RxDBLeaderElectionPlugin } from 'rxdb/plugins/leader-election';
 import { dev } from '$app/environment';
 import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
 
@@ -17,13 +19,13 @@ let dbInstance: RxDatabase<MyDatabaseCollections> | null = null;
  * Creates a new RxDatabase instance if it doesn't already exist.
  *
  * @param {string} [name='heda'] - The name of the database.
- * @param {boolean} [memory=false] - Whether to use memory storage.
+ * @param {boolean} [memory=true] - Whether to use memory storage.
  * @param {boolean} [validate_storage=false] - Whether to use validate storage.
  * @returns {Promise<RxDatabase>} The RxDatabase instance.
  */
 async function createDatabase(
 	name: string = 'heda',
-	memory: boolean = false,
+	memory: boolean = true,
 	validate_storage: boolean = false
 ): Promise<RxDatabase<MyDatabaseCollections>> {
 	if (dbInstance) {
@@ -33,6 +35,8 @@ async function createDatabase(
 		addRxPlugin(RxDBDevModePlugin);
 	}
 
+	addRxPlugin(RxDBLeaderElectionPlugin);
+	addRxPlugin(RxDBCleanupPlugin);
 	addRxPlugin(RxDBUpdatePlugin);
 	addRxPlugin(RxDBQueryBuilderPlugin);
 	dbInstance = await createRxDatabase({
@@ -54,7 +58,7 @@ async function createDatabase(
  * @returns {Promise<RxDatabase<MyDatabaseCollections>>} The initialized database instance.
  */
 export async function databaseInstance(name: string = 'heda',
-	memory: boolean = false,
+	memory: boolean = true,
 	validate_storage: boolean = false): Promise<RxDatabase<MyDatabaseCollections>> {
 	const database = await createDatabase(name, memory, validate_storage);
 
@@ -75,6 +79,5 @@ export async function databaseInstance(name: string = 'heda',
 			throw new Error('Failed to add collections');
 		}
 	}
-
 	return database;
 }
