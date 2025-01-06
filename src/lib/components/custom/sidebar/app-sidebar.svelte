9d7316f9e8a9746b62a0a1a02a70007ce5aa6102
Alessandro Benig <alebenig4@gmail.com>
2025-01-06 18:50:07 +0800

    ✨ Add confirmation dialog for removing selected nodes in sidebar

--- src/lib/components/custom/sidebar/app-sidebar.svelte
+++ src/lib/components/custom/sidebar/app-sidebar.svelte
@@ -12,6 +12,14 @@
 	import type { Project } from '@/db/schema';
 	import type { Node } from '@/db/schema';
 	import type { GenericPhaseMainLoadSchema } from '@/schema/load';
+	import { Trash, XCircle } from 'lucide-svelte';
+	import { getSelectNodesToDeleteState } from '@/hooks/select-nodes-to-delete.svelte';
+	import { removeNode } from '@/db/mutations';
+	import { getUndoRedoState } from '@/hooks/undo-redo.svelte';
+	import type { PhaseLoadSchedule } from '@/types/load/one_phase';
+	import { invalidate } from '$app/navigation';
+	import { ConfirmationDialog } from '..';
+	import { fly } from 'svelte/transition';
 
 	let {
 		ref = $bindable(null),
@@ -27,8 +35,61 @@
 		root_node: Node;
 	} = $props();
 
+	let is_confirmation_dialog_open = $state(false);
+	
 	const dialogs_state = getState<DialogState>(DIALOG_STATE_CTX);
+	const select_nodes_to_delete_state = getSelectNodesToDeleteState();
+	const undo_redo_state = getUndoRedoState();
+
+	async function handleRemoveSelectedItems() {
+		for (const node_id of select_nodes_to_delete_state.selected_nodes_id) {
+			const result = await removeNode(node_id);
+
+			if (result) {
+				undo_redo_state.setActionToUndo({
+					data: result?.removed_node as PhaseLoadSchedule,
+					action: 'delete_node',
+					children_nodes: result.children_nodes
+				});
+			}
+		}
+
+		await invalidate('app:workspace/load-schedule');
+		await invalidate('app:workspace');
+		select_nodes_to_delete_state.removeAllNodeIds();
+		is_confirmation_dialog_open = false;
+	}
 </script>
+
+{#if select_nodes_to_delete_state.selected_nodes_id.length > 0}
+	<div
+		out:fly={{ y: -50, duration: 300 }}
+		in:fly={{ y: -50, duration: 300 }}
+		class="fixed right-6 top-6 z-[30] rounded-[0.5rem] border bg-sidebar p-4"
+	>
+		<div class="flex items-center gap-2">
+			<Button
+				size="default"
+				onclick={() => select_nodes_to_delete_state.removeAllNodeIds()}
+				variant="outline"><XCircle />Unselect All Items</Button
+			>
+			<Button
+				size="default"
+				variant="destructive"
+				onclick={() => (is_confirmation_dialog_open = true)}
+			>
+				<Trash />Remove Selected Items
+			</Button>
+		</div>
+	</div>
+
+	<ConfirmationDialog
+		bind:open_dialog_state={is_confirmation_dialog_open}
+		trigger_text="Remove Items"
+		trigger_variant="destructive"
+		onConfirm={handleRemoveSelectedItems}
+	/>
+{/if}
 
 <Sidebar.Root bind:ref {...restProps}>
 	<SidebarHeader {project} {root_node} />
