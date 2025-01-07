0c9d6e25c5b3d73f749be126ea79b106ed0c3c7c
Alessandro Benig <alebenig4@gmail.com>
2025-01-07 15:54:23 +0800

    🔧 Add selected nodes actions component with delete functionality

---
+++ src/lib/components/custom/sidebar/(components)/selected-nodes-actions.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { Trash, XCircle } from 'lucide-svelte';
+	import { getSelectNodesToDeleteState } from '@/hooks/select-nodes-to-delete.svelte';
+	import { removeNode } from '@/db/mutations';
+	import { getUndoRedoState } from '@/hooks/undo-redo.svelte';
+	import type { PhaseLoadSchedule } from '@/types/load/one_phase';
+	import { invalidate } from '$app/navigation';
+	import { ConfirmationDialog } from '../..';
+	import { fly } from 'svelte/transition';
+	import Button from '@/components/ui/button/button.svelte';
+
+	let is_confirmation_dialog_open = $state(false);
+	const select_nodes_to_delete_state = getSelectNodesToDeleteState();
+	const undo_redo_state = getUndoRedoState();
+
+	async function handleRemoveSelectedItems() {
+		let results = [] as { data: PhaseLoadSchedule; children_nodes?: PhaseLoadSchedule[] }[];
+
+		for (const node_id of select_nodes_to_delete_state.selected_nodes_id) {
+			const result = await removeNode(node_id);
+
+			if (result) {
+				results = [
+					...results,
+					{ data: result.removed_node, children_nodes: result.children_nodes }
+				];
+			}
+		}
+
+		undo_redo_state.setActionToUndo({
+			action: 'batch_delete',
+			batch_data: results
+		});
+
+		await invalidate('app:workspace/load-schedule');
+		await invalidate('app:workspace');
+		select_nodes_to_delete_state.removeAllNodeIds();
+		is_confirmation_dialog_open = false;
+	}
+</script>
+
+{#if select_nodes_to_delete_state.selected_nodes_id.length > 0}
+	{@const count = select_nodes_to_delete_state.getSelectedNodeIdsCount()}
+	<div
+		out:fly={{ y: 50, duration: 300 }}
+		in:fly={{ y: 50, duration: 300 }}
+		class="fixed bottom-6 right-6 z-[30] rounded-[0.5rem] border bg-sidebar p-4"
+	>
+		<div class="flex items-center gap-2">
+			<Button
+				size="default"
+				onclick={() => select_nodes_to_delete_state.removeAllNodeIds()}
+				variant="outline"><XCircle />Unselect All {count} Items</Button
+			>
+			<Button
+				size="default"
+				variant="destructive"
+				onclick={() => (is_confirmation_dialog_open = true)}
+			>
+				<Trash />Remove Selected {count} Items
+			</Button>
+		</div>
+	</div>
+
+	<ConfirmationDialog
+		bind:open_dialog_state={is_confirmation_dialog_open}
+		trigger_text="Remove Items"
+		trigger_variant="destructive"
+		onConfirm={handleRemoveSelectedItems}
+	/>
+{/if}
