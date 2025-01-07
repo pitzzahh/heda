5ba042295c2fa7281603fb8603547cc94d4976f5
Alessandro Benig <alebenig4@gmail.com>
2025-01-07 14:24:48 +0800

    🔧 Add MultiCopyDialog component and fix export formatting in index.ts

--- src/lib/components/custom/index.ts
+++ src/lib/components/custom/index.ts
@@ -1,6 +1,7 @@
-export { default as HighestUnitForm } from './highest-unit-form.svelte'
-export { default as NewFileForm } from './new-file-form.svelte'
+export { default as HighestUnitForm } from './highest-unit-form.svelte';
+export { default as NewFileForm } from './new-file-form.svelte';
 export { default as PageProgress } from './page-progress.svelte';
 export { default as ConfirmationDialog } from './confirmation-dialog.svelte';
 export { default as ViewChangelog } from './view-changelog.svelte';
 export { default as Settings } from './settings.svelte';
+export { default as MultiCopyDialog } from './multi-copy-dialog.svelte';
