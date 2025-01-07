18c4be96c6158a53daaa7c6666c311a0cb5fec19
Alessandro Benig <alebenig4@gmail.com>
2025-01-07 15:54:51 +0800

    🔧 Export SelectedNodesActions component from sidebar index

--- src/lib/components/custom/sidebar/(components)/index.ts
+++ src/lib/components/custom/sidebar/(components)/index.ts
@@ -1,2 +1,3 @@
 export * from './sidebar-tree-icons.svelte';
 export { default as UndoRedoButtons } from './undo-redo-buttons.svelte';
+export { default as SelectedNodesActions } from './selected-nodes-actions.svelte';
