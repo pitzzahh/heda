733686caa9de3b6cce60fd6223e51cca54593c6d
Peter John Arao <pitzzahh@gmail.com>
2025-01-09 00:24:43 +0800

    ✨ Add logging for retrieving all projects in the database query

--- src/lib/db/queries/index.ts
+++ src/lib/db/queries/index.ts
@@ -29,7 +29,7 @@ export async function getCurrentProject(project_id?: string): Promise<Project | 
 export async function getAllProjects(fields?: (keyof Project)[]): Promise<Project[] | undefined> {
 	const db = await databaseInstance();
 	const projects = await db.projects.find().exec();
-
+	console.log('Getting all projects');
 	if (fields?.length) {
 		return projects.map((project) => {
 			const data = project._data as Project;
