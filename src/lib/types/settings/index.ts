32779b4511ca9f4ef1906bd90617cbd7c81413a1
Alessandro Benig <alebenig4@gmail.com>
2025-01-09 20:58:27 +0800

    ♻️ Add theme_mode property to Settings type for enhanced customization

--- src/lib/types/settings/index.ts
+++ src/lib/types/settings/index.ts
@@ -1,9 +1,10 @@
-import type { Font } from '@/hooks/settings-state.svelte';
+import type { Font, ThemeMode } from '@/hooks/settings-state.svelte';
 export type Settings = {
-  color: 'autocad' | 'excel';
-  font: Font,
-  show_loads_on_unit_hierarchy: boolean,
-  is_adjustment_factor_dynamic: boolean,
-  is_panel_multi_copy: boolean,
-  is_load_multi_copy: boolean,
+	color: 'autocad' | 'excel';
+	font: Font;
+	theme_mode: ThemeMode;
+	show_loads_on_unit_hierarchy: boolean;
+	is_adjustment_factor_dynamic: boolean;
+	is_panel_multi_copy: boolean;
+	is_load_multi_copy: boolean;
 };
