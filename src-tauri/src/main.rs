5b529c99dee452aca09f7da6d42afa136b9476e4
Peter John Arao <pitzzahh@gmail.com>
2025-01-09 22:12:34 +0800

    ✨ Add function to retrieve file name from path and integrate with Tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+
 #[tauri::command]
 fn get_env_var(key: String) -> String {
     std::env::var(String::from(key)).unwrap_or(String::from(""))
 }
 
+#[tauri::command]
+fn get_file_name(path: String) -> String {
+    let path = Path::new(path.as_str());
+    let filename = path.file_name().unwrap_or_default();
+    filename.to_str().unwrap().to_string()
+}
+
 fn main() {
     dotenv::load().ok();
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_log::Builder::new()
@@ -19,7 +27,10 @@ fn main() {
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![get_env_var])
+        .invoke_handler(tauri::generate_handler![get_file_name])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
