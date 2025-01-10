8ff1e4507fa439b43e039eed38fd77df2bb80f33
Peter John Arao <pitzzahh@gmail.com>
2025-01-11 00:52:49 +0800

    ➖ Remove plugin-fs-extra rust crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::time::SystemTime;
 
 #[tauri::command]
 fn get_env_var(key: String) -> String {
@@ -15,10 +17,18 @@ fn get_file_name(path: String) -> String {
     filename.to_str().unwrap().to_string()
 }
 
+#[tauri::command]
+fn get_file_metadata(path: String) -> Result<(u64, SystemTime), String> {
+    let path = PathBuf::from(path);
+    match fs::metadata(&path) {
+        Ok(metadata) => Ok((metadata.len(), metadata.modified().unwrap())),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 fn main() {
     dotenv::from_read(include_str!("../../.env").as_bytes()).unwrap().load();
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
@@ -27,9 +37,8 @@ fn main() {
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_fs_extra::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![get_env_var, get_file_name])
+        .invoke_handler(tauri::generate_handler![get_env_var, get_file_name, get_file_metadata])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
