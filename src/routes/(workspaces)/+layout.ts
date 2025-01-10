5648a212b3a4a273e720a3f7d213ac88ac31e7ea
Peter John Arao <pitzzahh@gmail.com>
2025-01-10 20:35:07 +0800

    ♻️ Refactor load function to streamline project retrieval and app passphrase handling

--- src/routes/(workspaces)/+layout.ts
+++ src/routes/(workspaces)/+layout.ts
@@ -4,16 +4,14 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { generic_phase_panel_schema } from '@/schema/panel';
 import { getCurrentProject, getRootNode } from '@/db/queries/index.js';
 import { generic_phase_main_load_schema } from '@/schema/load';
-import { BASE_DIR, generateUniqueFileName, getEnv } from '@/helpers/security/index.js';
+import { BASE_DIR, generateUniqueFileName } from '@/helpers/security/index.js';
 
-export const load = async ({ depends, url: { searchParams } }) => {
+export const load = async ({ parent, depends, url: { searchParams } }) => {
 	depends('app:workspace');
-	const project_id = searchParams.get('project_id') ?? undefined;
-	const project = (await getCurrentProject(project_id));
+	const project = (await getCurrentProject(searchParams.get('project_id') ?? undefined));
 	const root_node = (await getRootNode());
-	const app_pass_phrase = await getEnv('APP_PASS_PHRASE');
 
-	console.log({ project, root_node, app_pass_phrase });
+	const { app_pass_phrase } = await parent();
 
 	return {
 		is_new_file: searchParams.get('new_file') === 'true',
@@ -22,7 +20,6 @@ export const load = async ({ depends, url: { searchParams } }) => {
 		generic_phase_panel_form: await superValidate(zod(generic_phase_panel_schema)),
 		phase_main_load_form: await superValidate(zod(generic_phase_main_load_schema)),
 		project_title: project?.project_name ?? (await generateUniqueFileName(project?.project_name ?? "Untitled", BASE_DIR)).split('.')[0],
-		app_pass_phrase,
 		can_create_project: app_pass_phrase !== null,
 		project,
 		root_node
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,10 @@
+import { getEnv } from '@/helpers/security/index.js';
+
 export const prerender = true;
 export const ssr = false;
+
+export const load = async () => {
+  return {
+    app_pass_phrase: await getEnv('APP_PASS_PHRASE')
+  }
+}
