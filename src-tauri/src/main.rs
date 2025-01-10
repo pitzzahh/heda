20c2fe398259e82c02a9871f846dbb51bfcd4071
Peter John Arao <pitzzahh@gmail.com>
2025-01-10 21:02:28 +0800

    ♻️ Fix syntax error in main function by correcting parentheses placement

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,16 +9,17 @@ fn get_env_var(key: String) -> String {
 }
 
 #[tauri::command]
-fn rename_file(old_path: String, new_path: String) -> Result<(), String> {
-    std::fs::rename(old_path, new_path).map_err(|e| e.to_string())
-}
-
-#[tauri::command]
 fn get_file_name(path: String) -> String {
     let path = Path::new(path.as_str());
     let filename = path.file_stem().unwrap_or_default();
     filename.to_str().unwrap().to_string()
 }
+
+#[tauri::command]
+fn rename_file(old_path: String, new_path: String) -> Result<(), String> {
+    std::fs::rename(old_path, new_path).map_err(|e| e.to_string())
+}
+
 
 fn main() {
     let result = dotenv::from_read(include_str!("../../.env").as_bytes()).unwrap();
@@ -29,12 +30,12 @@ fn main() {
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
                     tauri_plugin_log::TargetKind::Stdout,
-        .invoke_handler(tauri::generate_handler![get_env_var, get_file_name, rename_file])
+                ))
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![get_env_var, get_file_name])
+        .invoke_handler(tauri::generate_handler![get_env_var, get_file_name, rename_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
