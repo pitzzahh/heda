5a93b23e1c548698ee3844d1e9f98c3f1ff5d5b9
Peter John Arao <pitzzahh@gmail.com>
2025-01-13 23:13:20 +0800

    ♻️ Initialize setProjectState in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,13 @@
 	import { ModeWatcher, mode } from 'mode-watcher';
 	import { Toaster } from '@/components/ui/sonner/index.js';
 	import { setState } from '@/state/index.svelte';
-	import type { DialogState, MiscState } from '@/state/types';
+	import type { DialogState } from '@/state/types';
 	import { DIALOG_STATE_CTX, MISC_STATE_CTX } from '@/state/constants';
 	import { setSettingsState } from '@/hooks/settings-state.svelte';
 	import { setUndoRedoState } from '@/hooks/undo-redo.svelte';
 	import { setSelectNodesToDeleteState } from '@/hooks/select-nodes-to-delete.svelte';
 	import { setCollapsiblesState } from '@/hooks/node-collapsibles.svelte';
+	import { setProjectState } from '@/hooks/project-state.svelte';
 
 	let { children } = $props();
 
@@ -17,18 +18,12 @@
 	setUndoRedoState();
 	setSelectNodesToDeleteState();
 	setCollapsiblesState();
+	setProjectState();
 	setState<DialogState>(
 		{
 			highestUnit: false
 		},
 		DIALOG_STATE_CTX
-	);
-
-	setState<MiscState>(
-		{
-			form_data: {}
-		},
-		MISC_STATE_CTX
 	);
 
 	$effect(() => {
