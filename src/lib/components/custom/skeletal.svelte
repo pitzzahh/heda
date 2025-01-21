4f6d4d854c945435733841c6000172cdf55327cf
Peter John Arao <pitzzahh@gmail.com>
2025-01-21 22:36:25 +0800

    ✨ Add skeletal component for improved loading state representation

---
+++ src/lib/components/custom/skeletal.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Skeleton } from '@/components/ui/skeleton';
+	import type { Snippet } from 'svelte';
+	let {
+		children,
+		options_count = Math.floor(Math.random() * (10 - 5 + 1)) + 5
+	}: {
+		children?: Snippet;
+		options_count?: number;
+	} = $props();
+</script>
+
+<div class="mt-2 flex flex-col gap-2">
+	<div class="flex items-center justify-between gap-2">
+		<div class="flex items-center justify-between gap-2">
+			<Skeleton class="h-9 w-72 rounded-md" />
+			{#each Array(options_count) as _}
+				<Skeleton class="h-8 w-[6.5rem] rounded-md" />
+			{/each}
+		</div>
+		<Skeleton class="h-9 w-[6.5rem] rounded-md md:w-20" />
+	</div>
+	{@render children?.()}
+	{#if !children}
+		<div class="flex flex-col items-center justify-center gap-1">
+			{#each Array(15) as _}
+				<Skeleton class="h-9 w-full rounded-md" />
+			{/each}
+		</div>
+
+		<div class="flex items-center justify-between gap-2">
+			<div class="flex-1 text-sm text-muted-foreground">
+				<Skeleton class="h-9 w-32 rounded-md" />
+			</div>
+
+			<div class="flex items-center justify-center gap-2">
+				<Skeleton class="h-9 w-40 rounded-md" />
+				<Skeleton class="h-9 w-28 rounded-md" />
+				{#each Array(4) as _}
+					<Skeleton class="h-9 w-9 rounded-md" />
+				{/each}
+			</div>
+		</div>
+	{/if}
+</div>
