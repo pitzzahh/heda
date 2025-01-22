7e318cf3789cfd2684f6931846c946858422fed4
Peter John Arao <pitzzahh@gmail.com>
2025-01-22 14:07:46 +0800

    ✨ Enhance app sidebar to include project title and loaded project ID for improved context

--- src/lib/components/custom/sidebar/app-sidebar.svelte
+++ src/lib/components/custom/sidebar/app-sidebar.svelte
@@ -14,6 +14,8 @@
 	import type { GenericPhaseMainLoadSchema } from '@/schema/load';
 	import { SelectedNodesActions } from './(components)';
 	import { toast } from 'svelte-sonner';
+	import { getCurrentProject, getRootNode } from '@/db/queries/index.js';
+	import { getProjectState } from '@/hooks/project-state.svelte';
 
 	let {
 		ref = $bindable(null),
@@ -22,6 +24,8 @@
 		project,
 		root_node,
 		app_pass_phrase,
+		loaded_project_id,
+		project_title,
 		file_encryption_salt,
 		can_create_project,
 		...restProps
@@ -30,12 +34,15 @@
 		phase_main_load_form: SuperValidated<GenericPhaseMainLoadSchema>;
 		project?: Project;
 		root_node: Node;
+		loaded_project_id: string | undefined;
+		project_title: string | undefined;
 		app_pass_phrase: string | null;
 		file_encryption_salt: string | null;
 		can_create_project: boolean;
 	} = $props();
 
 	const dialogs_state = getState<DialogState>(DIALOG_STATE_CTX);
+	const project_state = getProjectState();
 
 	let disable_create_project = $state(false);
 
@@ -53,46 +60,61 @@
 
 <SelectedNodesActions />
 <Sidebar.Root bind:ref {...restProps}>
-	<SidebarHeader {project} {root_node} {app_pass_phrase} {file_encryption_salt} />
+	<SidebarHeader
+		{project}
+		{root_node}
+		{loaded_project_id}
+		{project_title}
+		{app_pass_phrase}
+		{file_encryption_salt}
+	/>
 	<Sidebar.Content class="overflow-y-auto">
 		<Sidebar.Group>
 			<Sidebar.GroupLabel>System Hierarchy</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
-					{#if root_node?.highest_unit_form}
-						<svelte:boundary>
-							<SidebarTree
-								node={root_node}
-								highest_unit={root_node.highest_unit_form}
-								{phase_main_load_form}
-								{generic_phase_panel_form}
-								{project}
-							/>
+					{#if project_state.loaded}
+						{#await getCurrentProject(loaded_project_id, project_title) then current_project}
+							{#if root_node?.highest_unit_form}
+								<svelte:boundary>
+									<SidebarTree
+										node={root_node}
+										highest_unit={root_node.highest_unit_form}
+										{phase_main_load_form}
+										{generic_phase_panel_form}
+										{project}
+									/>
 
-							{#snippet failed(error, reset)}
-								<p class="text-sm text-muted-foreground">{error}</p>
-								<Button onclick={reset}>Something went horribly wrong. Click to FIX me</Button>
-							{/snippet}
-						</svelte:boundary>
-					{:else}
-						<div class="grid h-[85vh] place-content-center">
-							<div class="grid gap-2">
-								<div class="text-center">
-									<p class="text-lg font-bold text-muted-foreground">There is no project yet.</p>
-									<p class="text-sm text-muted-foreground">Create a new project to get started.</p>
+									{#snippet failed(error, reset)}
+										<p class="text-sm text-muted-foreground">{error}</p>
+										<Button onclick={reset}>Something went horribly wrong. Click to FIX me</Button>
+									{/snippet}
+								</svelte:boundary>
+							{:else}
+								<div class="grid h-[85vh] place-content-center">
+									<div class="grid gap-2">
+										<div class="text-center">
+											<p class="text-lg font-bold text-muted-foreground">
+												There is no project yet.
+											</p>
+											<p class="text-sm text-muted-foreground">
+												Create a new project to get started.
+											</p>
+										</div>
+
+										<!-- OPENS THE HIGHEST UNIT FORM IF THERE'S NO EXISTING PROJECT -->
+										<Button
+											size="sm"
+											disabled={disable_create_project}
+											onclick={() => (dialogs_state.highestUnit = true)}
+											href="/workspace?new_file=true"
+										>
+											<PlusIcon className="size-4 ml-3" /> Create a project
+										</Button>
+									</div>
 								</div>
-
-								<!-- OPENS THE HIGHEST UNIT FORM IF THERE'S NO EXISTING PROJECT -->
-								<Button
-									size="sm"
-									disabled={disable_create_project}
-									onclick={() => (dialogs_state.highestUnit = true)}
-									href="/workspace?new_file=true"
-								>
-									<PlusIcon className="size-4 ml-3" /> Create a project
-								</Button>
-							</div>
-						</div>
+							{/if}
+						{/await}
 					{/if}
 				</Sidebar.Menu>
 			</Sidebar.GroupContent>
