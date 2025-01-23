39c843306de16efbee11fe4454266664bcae165d
Peter John Arao <pitzzahh@gmail.com>
2025-01-23 21:15:59 +0800

    ♻️ Refactor undo-redo buttons to improve code clarity and maintainability

--- src/lib/components/custom/sidebar/(components)/undo-redo-buttons.svelte
+++ src/lib/components/custom/sidebar/(components)/undo-redo-buttons.svelte
@@ -4,20 +4,21 @@
 	import { Redo, Undo } from 'lucide-svelte';
 	import { getUndoRedoState } from '@/hooks/undo-redo.svelte';
 	import { cn } from '@/utils';
-
-	let undo_redo_state = getUndoRedoState();
+	const undo_redo_state = getUndoRedoState();
 </script>
 
 <Tooltip.Provider delayDuration={100}>
 	<Tooltip.Root>
 		<Tooltip.Trigger
-			class={buttonVariants({
-				variant: 'outline',
-				size: 'icon',
-				className: cn({
+			class={cn(
+				buttonVariants({
+					variant: 'outline',
+					size: 'icon'
+				}),
+				{
 					'cursor-not-allowed': !undo_redo_state.hasUndoActions()
-				})
-			})}
+				}
+			)}
 			onclick={() => undo_redo_state.undo()}
 			disabled={!undo_redo_state.hasUndoActions()}
 		>
@@ -30,13 +31,15 @@
 <Tooltip.Provider delayDuration={100}>
 	<Tooltip.Root>
 		<Tooltip.Trigger
-			class={buttonVariants({
-				variant: 'outline',
-				size: 'icon',
-				className: cn({
+			class={cn(
+				buttonVariants({
+					variant: 'outline',
+					size: 'icon'
+				}),
+				{
 					'cursor-not-allowed': !undo_redo_state.hasRedoActions()
-				})
-			})}
+				}
+			)}
 			onclick={() => undo_redo_state.redo()}
 			disabled={!undo_redo_state.hasRedoActions()}
 		>
