4283447f877f63f1f0272b6033beeef3fe2748b4
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 00:02:10 +0800

    ♻️ Update undo-redo functionality to include current project name for improved context in operations

--- src/lib/components/custom/undo-redo-wrapper.svelte
+++ src/lib/components/custom/undo-redo-wrapper.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import { getUndoRedoState } from '@/hooks/undo-redo.svelte';
+	import { getProjectState } from '@/hooks/project-state.svelte';
 
 	let { children } = $props();
 	let undo_redo_state = getUndoRedoState();
+	const project_state = getProjectState();
 
 	function handleKeyDown(event: KeyboardEvent) {
 		if (event.ctrlKey) {
 			switch (event.key.toLocaleLowerCase()) {
 				case 'z':
 					event.preventDefault();
-					undo_redo_state.undo();
+					undo_redo_state.undo(project_state.current_project_name);
 					break;
 				case 'y':
 					event.preventDefault();
-					undo_redo_state.redo();
+					undo_redo_state.redo(project_state.current_project_name);
 					break;
 			}
 		}
