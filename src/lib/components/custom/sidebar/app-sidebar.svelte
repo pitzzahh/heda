1d12ea6caf039222f9dadcdb7acf22579cc0edfa
Peter John Arao <pitzzahh@gmail.com>
2025-01-23 22:51:22 +0800

    ♻️ Update project name handling in sidebar component for consistency and improve error message styling

--- src/lib/components/custom/sidebar/app-sidebar.svelte
+++ src/lib/components/custom/sidebar/app-sidebar.svelte
@@ -61,7 +61,7 @@
 			<Sidebar.GroupLabel>System Hierarchy</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
-					{#await Promise.all( [getCurrentProject(loaded_project_id), getRootNode()] ) then [project, root_node]}
+					{#await Promise.all( [getCurrentProject(project_state.current_project_name, loaded_project_id), getRootNode(project_state.current_project_name)] ) then [project, root_node]}
 						{#if root_node?.highest_unit_form}
 							<svelte:boundary>
 								<SidebarTree
@@ -73,7 +73,7 @@
 								/>
 
 								{#snippet failed(error, reset)}
-									<p class="text-sm text-muted-foreground">{error}</p>
+									<p class="text-muted-foreground text-sm">{error}</p>
 									<Button onclick={reset}>Something went horribly wrong. Click to FIX me</Button>
 								{/snippet}
 							</svelte:boundary>
@@ -82,8 +82,8 @@
 					<div class="grid h-[85vh] place-content-center">
 						<div class="grid gap-2">
 							<div class="text-center">
-								<p class="text-lg font-bold text-muted-foreground">There is no project yet.</p>
-								<p class="text-sm text-muted-foreground">Create a new project to get started.</p>
+								<p class="text-muted-foreground text-lg font-bold">There is no project yet.</p>
+								<p class="text-muted-foreground text-sm">Create a new project to get started.</p>
 							</div>
 
 							<!-- OPENS THE HIGHEST UNIT FORM IF THERE'S NO EXISTING PROJECT -->
