289f53124c2f742fc06b0bb3565fbf88f76f0042
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 23:20:17 +0800

    ♻️ Refactor selected_nodes_id management in SelectNodesToDelete class to use array methods for improved functionality and clarity

--- src/lib/hooks/select-nodes-to-delete.svelte.ts
+++ src/lib/hooks/select-nodes-to-delete.svelte.ts
@@ -2,32 +2,30 @@ import { setState, getState } from '@/state/index.svelte';
 import { SELECT_NODES_TO_DELETE_CTX } from '@/state/constants';
 
 export class SelectNodesToDelete {
-	selected_nodes_id: Set<string>;
+	selected_nodes_id = $state<string[]>([]);
 
-	constructor() {
-		this.selected_nodes_id = $state(new Set<string>());
-	}
+	constructor() { }
 
 	addOrRemoveNodeId(id: string) {
 		if (this.checkIsIdSelected(id)) {
 			this.removeNodeId(id);
-		} else this.selected_nodes_id.add(id);
+		} else this.selected_nodes_id = [...this.selected_nodes_id, id];
 	}
 
 	checkIsIdSelected(id: string) {
-		return this.selected_nodes_id.has(id);
+		return this.selected_nodes_id.includes(id);
 	}
 
 	removeNodeId(id: string) {
-		this.selected_nodes_id.delete(id);
+		this.selected_nodes_id = this.selected_nodes_id.filter((node_id) => node_id !== id);
 	}
 
 	getSelectedNodeIdsCount() {
-		return this.selected_nodes_id.size;
+		return this.selected_nodes_id.length
 	}
 
 	removeAllNodeIds() {
-		this.selected_nodes_id.clear();
+		this.selected_nodes_id = [];
 	}
 }
 
