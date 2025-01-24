f9067526f3217b224d29bfcdc83834eb99b3105c
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 20:14:20 +0800

    🔥 Remove unused load functions and streamline return values

--- src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
+++ src/routes/(workspaces)/workspace/load-schedule/[id]/+page.ts
@@ -5,8 +5,6 @@ import {
 	getCurrentProject,
 	getNodeById,
 	getRootNode,
-	getComputedLoads,
-	getComputedVoltageDrops
 } from '@/db/queries/index.js';
 import { goto } from '$app/navigation';
 import type { Node } from '@/db/schema';
@@ -25,14 +23,10 @@ export const load = async ({ depends, params }) => {
 	}
 	const current_node = await getNodeById(node_id);
 	if (!current_node) goto('/workspace');
-	const nodes = await getComputedLoads(node_id);
-	const voltage_drops = await getComputedVoltageDrops()
 	return {
 		phase_main_load_form: await superValidate(zod(generic_phase_main_load_schema)),
 		project,
-		nodes: nodes && nodes?.length > 0 ? nodes : [],
 		root_node: root_node as Node,
-		current_node,
-		voltage_drops
+		current_node
 	};
 };
