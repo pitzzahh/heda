57fac4b48e639e06fd5f29b5eed629ed8060b420
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 14:18:25 +0800

    ♻️ Refactor load function in +layout.ts to implement redirect for improved navigation based on current_project and root_node

--- src/routes/(workspaces)/+layout.ts
+++ src/routes/(workspaces)/+layout.ts
@@ -5,14 +5,19 @@ import { generic_phase_panel_schema } from '@/schema/panel';
 import { generic_phase_main_load_schema } from '@/schema/load';
 import { getEnv } from '@/helpers/security/index.js';
 import { getCurrentProject, getRootNode } from '@/db/queries/index.js';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ depends, url: { searchParams } }) => {
 	depends('app:workspace');
 	const app_pass_phrase = await getEnv('APP_PASS_PHRASE');
 	const file_encryption_salt = await getEnv('FILE_ENCRYPTION_SALT');
 	const instance_name = searchParams.get('instance_name');
+	const distribution_unit = searchParams.get('distribution_unit');
 	const current_project = instance_name ? await getCurrentProject(instance_name) : null;
 	const root_node = instance_name ? await getRootNode(instance_name) : null;
+	if (current_project && root_node) {
+		return redirect(303, `/workspace/load-schedule/${distribution_unit as string}_${root_node.id}?project_title=${instance_name}`)
+	}
 	return {
 		is_new_file: searchParams.get('new_file') === 'true',
 		is_load_file: searchParams.get('load_file') === 'true',
