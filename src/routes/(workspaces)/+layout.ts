7e0ae8d04129debb4d9cd020ac09082565d4393e
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 14:10:31 +0800

    ♻️ Refactor load function in +layout.ts to retrieve current_project and root_node for improved data handling

--- src/routes/(workspaces)/+layout.ts
+++ src/routes/(workspaces)/+layout.ts
@@ -4,11 +4,15 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { generic_phase_panel_schema } from '@/schema/panel';
 import { generic_phase_main_load_schema } from '@/schema/load';
 import { getEnv } from '@/helpers/security/index.js';
+import { getCurrentProject, getRootNode } from '@/db/queries/index.js';
 
 export const load = async ({ depends, url: { searchParams } }) => {
 	depends('app:workspace');
 	const app_pass_phrase = await getEnv('APP_PASS_PHRASE');
 	const file_encryption_salt = await getEnv('FILE_ENCRYPTION_SALT');
+	const instance_name = searchParams.get('instance_name');
+	const current_project = instance_name ? await getCurrentProject(instance_name) : null;
+	const root_node = instance_name ? await getRootNode(instance_name) : null;
 	return {
 		is_new_file: searchParams.get('new_file') === 'true',
 		is_load_file: searchParams.get('load_file') === 'true',
@@ -17,6 +21,8 @@ export const load = async ({ depends, url: { searchParams } }) => {
 		phase_main_load_form: await superValidate(zod(generic_phase_main_load_schema)),
 		app_pass_phrase,
 		file_encryption_salt,
-		can_create_project: app_pass_phrase !== null && file_encryption_salt !== null
+		can_create_project: app_pass_phrase !== null && file_encryption_salt !== null,
+		current_project,
+		root_node,
 	};
 };
