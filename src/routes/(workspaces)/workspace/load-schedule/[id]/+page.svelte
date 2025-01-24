5a2834bba5bd977eed8314b210afb0e605f3b196
Peter John Arao <pitzzahh@gmail.com>
2025-01-24 21:15:09 +0800

    ✨ Enhance loading experience with user notifications and refactor data fetching in load schedule component

--- src/routes/(workspaces)/workspace/load-schedule/[id]/+page.svelte
+++ src/routes/(workspaces)/workspace/load-schedule/[id]/+page.svelte
@@ -6,30 +6,29 @@
 	import { getComputedLoads, getComputedVoltageDrops, getNodeById } from '@/db/queries/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
 	import { voltageDropColumns } from '@/components/custom/table/voltage-drop-cols/voltage-drop-cols.js';
-	import type { NodeByIdResult } from '@/types/db/index.js';
 	import { Skeletal } from '@/components/custom/index.js';
+	import { getProjectState } from '@/hooks/project-state.svelte.js';
+	import { toast } from 'svelte-sonner';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
 
 	const { root_node } = data;
 	const params = $derived(page.params);
-
-	let supply_from_name = $state('');
-	let node: NodeByIdResult | null = $state(null);
+	const node_id = $derived(params.id.split('_').at(-1) as string);
+	const project_state = getProjectState();
 
 	$effect(() => {
-		const nodeId = params.id.split('_').at(-1) as string;
-
-		getNodeById(nodeId).then((current_node) => {
-			const parentId = current_node?.parent_id;
-			node = current_node;
-			if (parentId) {
-				getNodeById(parentId).then((node) => {
-					supply_from_name =
-						node?.panel_data?.name || node?.highest_unit_form?.distribution_unit || '--';
-				});
-			} else supply_from_name = '--';
+		toast.info('Loading...', {
+			description: 'Please wait while the data is being fetched.',
+			position: 'top-center'
 		});
+		if (!project_state.loaded) {
+			toast.warning('Project not loaded yet. Redirecting to workspaces...', {
+				description: 'Please create a project or load an existing one first.'
+			});
+			goto(`/workspaces`);
+		}
 	});
 </script>
 
@@ -37,11 +36,15 @@
 	<div class="grid grid-cols-2">
 		<div>
 			<p class="font-semibold">
-				Distribution Unit: <span class="font-normal"
-					>{node?.panel_data?.name ??
-						node?.highest_unit_form?.distribution_unit ??
-						'NOT FOUND'}</span
-				>
+				Distribution Unit: <span class="font-normal">
+					{#await getNodeById(node_id)}
+						Loading...
+					{:then current_node}
+						{current_node?.panel_data?.name ??
+							current_node?.highest_unit_form?.distribution_unit ??
+							'NOT FOUND'}
+					{/await}
+				</span>
 			</p>
 			<p class="font-semibold">
 				Phase: <span class="font-normal">{root_node?.highest_unit_form?.phase ?? ''}</span>
@@ -51,7 +54,22 @@
 		<p class="font-semibold">
 			Supply From:
 			<span class="font-normal">
-				{supply_from_name}
+				{#await getNodeById(node_id)}
+					Loading...
+				{:then current_node}
+					{@const parent_id = current_node?.parent_id}
+					{#if parent_id}
+						{#await getNodeById(parent_id)}
+							Loading...
+						{:then parent_node}
+							{parent_node?.panel_data?.name ||
+								parent_node?.highest_unit_form?.distribution_unit ||
+								'--'}
+						{/await}
+					{:else}
+						--
+					{/if}
+				{/await}
 			</span>
 		</p>
 	</div>
@@ -61,22 +79,20 @@
 			<Tabs.Trigger value="voltage-drop">Voltage Drop</Tabs.Trigger>
 		</Tabs.List>
 		<Tabs.Content value="load-sched">
-			{#if node?.id}
-				{#await getComputedLoads(node?.id as string)}
-					<Skeletal options_count={10} />
-				{:then loads}
-					<DataTable
-						data={loads && loads.length > 0 ? (loads as PhaseLoadSchedule[]) : []}
-						columns={onePhaseMainOrWyeCols(
-							data.phase_main_load_form,
-							data.current_node as PhaseLoadSchedule,
-							root_node?.highest_unit_form,
-							loads && loads.length > 0 ? loads.at(-1) : undefined
-						)}
-						is_footer_shown={data.current_node?.node_type !== 'root'}
-					/>
-				{/await}
-			{/if}
+			{#await Promise.all([getNodeById(node_id), getComputedLoads(node_id as string)])}
+				<Skeletal options_count={10} />
+			{:then [current_node, loads]}
+				<DataTable
+					data={loads && loads.length > 0 ? (loads as PhaseLoadSchedule[]) : []}
+					columns={onePhaseMainOrWyeCols(
+						data.phase_main_load_form,
+						current_node as PhaseLoadSchedule,
+						root_node?.highest_unit_form,
+						loads && loads.length > 0 ? loads.at(-1) : undefined
+					)}
+					is_footer_shown={current_node?.node_type !== 'root'}
+				/>
+			{/await}
 		</Tabs.Content>
 		<Tabs.Content value="voltage-drop">
 			{#await getComputedVoltageDrops()}
